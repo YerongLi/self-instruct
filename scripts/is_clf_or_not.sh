77f7fe333e39076b5b47a546f818597a47bb5735
Yerong <yerong.li@outlook.com>
2023-08-10 17:00:55 -0500

    Use batch size 1

--- scripts/is_clf_or_not.sh
+++ scripts/is_clf_or_not.sh
@@ -3,4 +3,4 @@ batch_dir=data/gpt3_generations/
 python self_instruct/identify_clf_or_not.py \
     --batch_dir ${batch_dir} \
     --engine "davinci" \
-    --request_batch_size 5
+    --request_batch_size 1
