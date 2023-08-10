fa12651aa370b77132d6b785921a828c89126db7
Yerong <yerong.li@outlook.com>
2023-08-10 16:08:32 -0500

    Missing comma

--- self_instruct/bootstrap_instructions.py
+++ self_instruct/bootstrap_instructions.py
@@ -59,7 +59,7 @@ def run_llama_command(input_string, gpt3=True):
             },
             "choices": [
                 {
-                    "text": run_llama_command(input_string, False)
+                    "text": run_llama_command(input_string, False),
                     "finish_reason": "stop",
                     "index": 0
                 }
