3a405ee30d63f6dda98b3392a8e348ece3183cb9
Yerong <yerong.li@outlook.com>
2023-08-28 16:51:39 -0500

    Remove printing

--- self_instruct/generate_instances_ie.py
+++ self_instruct/generate_instances_ie.py
@@ -205,8 +205,6 @@ if __name__ == '__main__':
                 ]
 
                 def extract_text_between_markers(result, start_marker, end_marker):
-                    print(result)
-                    print('RESULT')
                     input_string = result['response']["choices"][0]["text"]
                     start_index = input_string.find(start_marker)
                     end_index = input_string.find(end_marker)
