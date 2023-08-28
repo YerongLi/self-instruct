c0f293e4e8903d9f66afc7128f4e1ea332139bbc
Yerong <yerong.li@outlook.com>
2023-08-28 15:48:42 -0500

    Update

--- self_instruct/generate_instances_ie.py
+++ self_instruct/generate_instances_ie.py
@@ -152,10 +152,10 @@ if __name__ == '__main__':
             tasks.append(data)
 
     task_clf_types = {}
-    with open(os.path.join(args.batch_dir, "is_clf_or_not_davinci_template_1.jsonl")) as fin:
-        for line in fin:
-            data = json.loads(line)
-            task_clf_types[data["instruction"]] = data["is_classification"].strip() in ["Yes", "yes", "YES"]
+    # with open(os.path.join(args.batch_dir, "is_clf_or_not_davinci_template_1.jsonl")) as fin:
+    #     for line in fin:
+    #         data = json.loads(line)
+    #         task_clf_types[data["instruction"]] = data["is_classification"].strip() in ["Yes", "yes", "YES"]
 
     if args.classification_tasks_only:
         tasks = [task for task in tasks if task_clf_types[task["instruction"]]]
