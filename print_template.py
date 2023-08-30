01040e84a0dec3b0b56ee5f3f769374719402182
Yerong <yerong.li@outlook.com>
2023-08-30 13:03:32 -0500

    out of range error

--- print_template.py
+++ print_template.py
@@ -21,18 +21,32 @@ for idx, task in enumerate(selected_tasks):
     schema = task['schema']
     
     # Find the schema part in the instruction
-    schema_start_idx = schema.find('Output format should be "') + len('Output format should be "')
+    schema_start_idx = schema.find('Output format is "') + len('Output format is "')
     schema_end_idx = schema.find('"', schema_start_idx)
     
     if schema_start_idx != -1 and schema_end_idx != -1:
         schema_part = schema[schema_start_idx:schema_end_idx]
-        relations_and_words = [item.strip() for item in schema_part.split(';')]
+        options_start_idx = schema_part.find('Option: ') + len('Option: ')
+        options_end_idx = schema_part.find('\n', options_start_idx)
+        options = schema_part[options_start_idx:options_end_idx].split(', ')
+        
+        text_start_idx = schema_part.find('Text: {0}') + len('Text: {0}\n')
+        text_end_idx = schema_part.find('\n', text_start_idx)
+        text = schema_part[text_start_idx:text_end_idx]
+        
+        output_start_idx = schema.find('output": "') + len('output": "')
+        output_end_idx = schema.find('", "is_classification":', output_start_idx)
+        output = schema[output_start_idx:output_end_idx]
         
         print(f"Task {idx + 1}: {task_name}")
+        print("Text:", text)
         print("Output:")
-        for relation_and_words in relations_and_words:
-            relation, words = [item.strip() for item in relation_and_words.split(':')]
-            word_list = [word.strip() for word in words.split(',')]
-            for word in word_list:
-                print(f"- {word} (Relation: {relation})")
+        
+        for option in options:
+            option_parts = option.split(': ')
+            option_type = option_parts[0]
+            option_words = [word.strip() for word in option_parts[1].split(', ')]
+            for word in option_words:
+                print(f"- {word} ({option_type})")
+        print("Answer:", output)
         print()
