5fcbcf038ed8cbd594743b0e8e2145f3a2f3e316
Yerong <yerong.li@outlook.com>
2023-08-30 00:52:00 -0500

    Customized prompt

--- self_instruct/generate_instances_ie.py
+++ self_instruct/generate_instances_ie.py
@@ -217,8 +217,7 @@ if __name__ == '__main__':
 
                 # Example usage
                 # input_string = "Yann LeCun, Yoshua Bengio\nOutput: Alan Turing\n\nTask: Extract information from text."
-                start_marker = "Yann LeCun, Yoshua Bengio"
-                end_marker = "Output: Alan Turing"
+                end_marker = input_first_template_for_gen[:10] ## TODO
 
                 results = [package(remove_prefix_markers(result["response"]["choices"][0]["text"], end_marker))
                     for result in results]
