40e43b2eda43ebb8c0c21f5bcabf9fbbf7c291dd
Yerong <yerong.li@outlook.com>
2023-08-30 14:37:57 -0500

    Remove new line

--- self_instruct/generate_instances.py
+++ self_instruct/generate_instances.py
@@ -32,7 +32,7 @@ def run_llama_command(input_string, gpt3=True):
             "-t",
             "1",
             "-n",
-            "2048",
+            "300",
             "--temp",
             "0.1",
             "--top-p",
