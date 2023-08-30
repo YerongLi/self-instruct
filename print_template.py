8fcafede11327c42ab7fe3a7904a42c1a2fe74c7
Yerong <yerong.li@outlook.com>
2023-08-30 13:00:11 -0500

    out of range error

--- print_template.py
+++ print_template.py
@@ -19,12 +19,18 @@ selected_tasks = random.sample(tasks_with_schema, k=10)
 for idx, task in enumerate(selected_tasks):
     task_name = task['name']
     schema = task['schema']
-    output_parts = [part.strip() for part in schema.split('Output format should be "')[1].split(';')[0].split('"')[1].split(':')]
-    relation = output_parts[0].strip()
-    words = [word.strip() for word in output_parts[1].split(',')]
+    output_format_start = "Output format should be \""
+    output_format_end = "\"."
     
-    print(f"Task {idx + 1}: {task_name}")
-    print("Output:")
-    for word in words:
-        print(f"- {word}")
-    print()
+    output_format_start_idx = schema.find(output_format_start)
+    output_format_end_idx = schema.find(output_format_end, output_format_start_idx)
+    
+    if output_format_start_idx != -1 and output_format_end_idx != -1:
+        output_format = schema[output_format_start_idx + len(output_format_start):output_format_end_idx]
+        output_parts = [part.strip() for part in output_format.split(';')[0].split(':')]
+        
+        print(f"Task {idx + 1}: {task_name}")
+        print("Output:")
+        for word in output_parts[1:]:
+            print(f"- {word}")
+        print()
