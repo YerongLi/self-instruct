3b6a198c874ef392aaad10f8cef4dd37f1e42b70
Yerong <yerong.li@outlook.com>
2023-08-30 13:25:51 -0500

    Don't search

--- print_template.py
+++ print_template.py
@@ -18,36 +18,21 @@ selected_tasks = random.sample(tasks_with_schema, k=10)
 # Generate and print examples
 for idx, task in enumerate(selected_tasks):
     task_name = task['name']
+    input_text = task['input']
     schema = task['schema']
     
-    # Find the schema part in the instruction
-    schema_start_idx = schema.find('Output format is "') + len('Output format is "')
-    schema_end_idx = schema.find('"', schema_start_idx)
+    output_start_idx = schema.find('output": "') + len('output": "')
+    output_end_idx = schema.find('", "is_classification":', output_start_idx)
+    output = schema[output_start_idx:output_end_idx]
     
-    if schema_start_idx != -1 and schema_end_idx != -1:
-        schema_part = schema[schema_start_idx:schema_end_idx]
-        options_start_idx = schema_part.find('Option: ') + len('Option: ')
-        options_end_idx = schema_part.find('\n', options_start_idx)
-        options = schema_part[options_start_idx:options_end_idx].split(', ')
-        
-        input_start_idx = schema_part.find('Input: {0}') + len('Input: {0}\n')
-        input_end_idx = schema_part.find('\n', input_start_idx)
-        input_text = schema_part[input_start_idx:input_end_idx]
-        
-        output_start_idx = schema.find('output": "') + len('output": "')
-        output_end_idx = schema.find('", "is_classification":', output_start_idx)
-        output = schema[output_start_idx:output_end_idx]
-        
-        print(f"Task {idx + 1}: {task_name}")
-        print("Input text:", input_text)
-        print("Output:")
-        
-        for option in options:
-            option_parts = option.split(': ')
-            if len(option_parts) > 1:
-                option_type = option_parts[0]
-                option_words = [word.strip() for word in option_parts[1].split(', ')]
-                for word in option_words:
-                    print(f"- {word} ({option_type})")
-        print("Answer:", output)
-        print()
+    print(f"Task {idx + 1}: {task_name}")
+    print("Input text:", input_text)
+    print("Output:")
+    
+    relation_word_pairs = output.split('; ')
+    for relation_word_pair in relation_word_pairs:
+        relation, words = relation_word_pair.split(': ')
+        word_list = [word.strip() for word in words.split(', ')]
+        for word in word_list:
+            print(f"- {word} (Relation: {relation})")
+    print()
