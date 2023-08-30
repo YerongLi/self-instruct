2acf12297b7230daad408fb38277a7fb3c2fd8ed
Yerong <yerong.li@outlook.com>
2023-08-30 13:26:34 -0500

    Don't search

--- print_template.py
+++ print_template.py
@@ -19,17 +19,15 @@ selected_tasks = random.sample(tasks_with_schema, k=10)
 for idx, task in enumerate(selected_tasks):
     task_name = task['name']
     input_text = task['input']
-    schema = task['schema']
+    output_text = task['output']
     
-    output_start_idx = schema.find('output": "') + len('output": "')
-    output_end_idx = schema.find('", "is_classification":', output_start_idx)
-    output = schema[output_start_idx:output_end_idx]
+    # Parse the output text to extract relation and word pairs
+    relation_word_pairs = [item.strip() for item in output_text.split(';')]
     
     print(f"Task {idx + 1}: {task_name}")
     print("Input text:", input_text)
     print("Output:")
     
-    relation_word_pairs = output.split('; ')
     for relation_word_pair in relation_word_pairs:
         relation, words = relation_word_pair.split(': ')
         word_list = [word.strip() for word in words.split(', ')]
