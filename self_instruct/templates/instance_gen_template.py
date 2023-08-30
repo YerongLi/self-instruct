42c5d55960f0cad8320d23d51ed0f2df9c6a1453
Yerong <yerong.li@outlook.com>
2023-08-30 14:39:41 -0500

    Remove new line

--- self_instruct/templates/instance_gen_template.py
+++ self_instruct/templates/instance_gen_template.py
@@ -33,7 +33,7 @@ Task:'''
 
 input_first_template_for_gen = '''Come up with examples for the following tasks. Try to generate multiple examples when possible. If the task doesn't require additional input, you can generate the output directly.
 
-Task: Please list all entity words in the text that fit the category. Output format is. 
+Task: Please list all entity words in the text that fit the category. Output format is 'type1: word1; type2: word2'. 
 Option: GENE
 Output:
 - Lying Leg Raises
