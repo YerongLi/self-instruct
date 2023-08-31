3c687d43992470ca90957a9326ec3617cc0c8372
Yerong <yerong.li@outlook.com>
2023-08-31 09:07:02 -0500

    Truncate at the second Task:

--- print_raw.py
+++ print_raw.py
@@ -4,10 +4,19 @@ import json
 with open('data/ie/70machine_generated_instances.jsonl', 'r') as file:
     lines = file.readlines()
 
-# Parse JSONL and print raw_instances
-for line in lines:
-    data = json.loads(line)
-    raw_instances = data.get('raw_instances')
-    if raw_instances:
-        print(raw_instances)
-        print("=" * 80)  # Separator
+# Create a file to write the output
+with open('text.txt', 'w') as output_file:
+    # Parse JSONL and write truncated raw_instances to the file
+    for line in lines:
+        data = json.loads(line)
+        raw_instances = data.get('raw_instances')
+        if raw_instances:
+            # Find the second occurrence of "Task:"
+            index = raw_instances.find("Task:", raw_instances.find("Task:") + 1)
+            if index != -1:
+                truncated_raw_instances = raw_instances[:index]
+            else:
+                truncated_raw_instances = raw_instances
+
+            output_file.write(truncated_raw_instances + "\n")
+            output_file.write("=" * 80 + "\n")
