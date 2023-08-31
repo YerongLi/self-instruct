26aa9b1c48d42bfd8dd5ade217724d0529d2ddeb
Yerong <yerong.li@outlook.com>
2023-08-31 02:48:57 -0500

    Print the raw instances

--- print_raw.py
+++ print_raw.py
@@ -1,18 +1,13 @@
+import json
+
 # Read the JSONL file
 with open('data/ie/machine_generated_instances.jsonl', 'r') as file:
-    lines = file.read().split('\n')
+    lines = file.readlines()
 
-# Separate instances and print
-current_instance = ""
+# Parse JSONL and print raw_instances
 for line in lines:
-    if line.strip() != "":
-        current_instance += line + "\n"
-    else:
-        if current_instance.strip() != "":
-            print(current_instance.strip())
-            print("\n" + "=" * 80 + "\n")
-            current_instance = ""
-
-# Print the last instance if there's any left
-if current_instance.strip() != "":
-    print(current_instance.strip())
+    data = json.loads(line)
+    raw_instances = data.get('raw_instances')
+    if raw_instances:
+        print(raw_instances)
+        print("=" * 80)  # Separator
