3b1f1e62411cef2b67f71ffeaced9ccf733c5926
Yerong <yerong.li@outlook.com>
2023-08-31 22:55:44 -0500

    Update

--- scripts/generate_instances.sh
+++ scripts/generate_instances.sh
@@ -1,5 +1,13 @@
+#!/bin/bash
+
 batch_dir=data/gpt3_generations/
 git pull; rm data/gpt3_generations/machine_generated_instances.jsonl ;
+
+test_flag=""
+if [[ $1 == "test" ]]; then
+    test_flag="--test"
+fi
+
 python self_instruct/generate_instances.py \
     --batch_dir ${batch_dir} \
     --input_file machine_generated_instructions.jsonl \
@@ -7,4 +15,4 @@ python self_instruct/generate_instances.py \
     --max_instances_to_gen 5 \
     --engine "davinci" \
     --request_batch_size 1 \
-    --test
+    ${test_flag}
