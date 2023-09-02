fd811435841f13d7de52792f9cc06ba4f901f93f
Yerong <yerong.li@outlook.com>
2023-09-02 00:21:44 -0500

    Use underline instead of the minus sign

--- cpp.py
+++ cpp.py
@@ -1,4 +1,4 @@
 from llama_cpp import Llama
-llm = Llama(model_path="/scratch/yerong/.cache/pyllama/Llama-2-7b/ggml-model-q4_0.gguf", n_gpu_layers= 100, n_ctx=4096)
+llm = Llama(model_path="/scratch/yerong/.cache/pyllama/Llama-2-7b/ggml-model-q4_0.gguf", n_gpu_layers= 100, n_ctx=100)
 output = llm("Q: Name the planets in the solar system? A: ", max_tokens=32, stop=["Q:", "\n"], echo=True)
 print(output)
