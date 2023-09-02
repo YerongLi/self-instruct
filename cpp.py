7f79533ab7d7a7f089895c9f6ca1b18630300055
Yerong <yerong.li@outlook.com>
2023-09-02 00:12:23 -0500

    Offload layers to GPU

--- cpp.py
+++ cpp.py
@@ -1,4 +1,4 @@
 from llama_cpp import Llama
 llm = Llama(model_path="/scratch/yerong/.cache/pyllama/Llama-2-7b/ggml-model-q4_0.gguf")
-output = llm("Q: Name the planets in the solar system? A: ", max_tokens=32, n-gpu-layers= 100, stop=["Q:", "\n"],, echo=True)
+output = llm("Q: Name the planets in the solar system? A: ", max_tokens=32, n_gpu_layers= 100, stop=["Q:", "\n"],, echo=True)
 print(output)
