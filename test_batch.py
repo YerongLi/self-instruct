aeb4f9b9a006fd6a22e9da3fb681c54bca00538b
Yerong <yerong.li@outlook.com>
2023-09-03 04:02:17 -0500

    Use original code

--- test_batch.py
+++ test_batch.py
@@ -3,9 +3,20 @@ from auto_gptq import AutoGPTQForCausalLM
 from transformers import AutoTokenizer
 
 # Load the AutoGPTQForCausalLM model and tokenizer
-model_name_or_path = "/scratch/yerong/.cache/pyllama/Llama-2-7B-GPTQ"
-model = AutoGPTQForCausalLM.from_quantized(model_name_or_path, model_basename="model", inject_fused_attention=False, use_safetensors=True, trust_remote_code=False, device="cuda:0", use_triton=False, quantize_config=None)
+model_name_or_path = "TheBloke/Llama-2-7B-GPTQ"
+model_basename = "model"
+
+use_triton = False
+
 tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, use_fast=True)
+
+model = AutoGPTQForCausalLM.from_quantized(model_name_or_path,
+        model_basename=model_basename,
+        use_safetensors=True,
+        trust_remote_code=True,
+        device="cuda:0",
+        use_triton=use_triton,
+        quantize_config=None)
 
 # Set the padding token to be equal to the end of sequence token (eos_token)
 tokenizer.pad_token = tokenizer.eos_token
@@ -41,5 +52,5 @@ with torch.no_grad():
         top_k=40
     )
 
-print(tokenizer.batch_decode(outputs[:, input_ids.shape[1]:], max_new_tokens=256, skip_special_tokens=True))
+# print(tokenizer.batch_decode(outputs[:, input_ids.shape[1]:], max_new_tokens=256, skip_special_tokens=True))
 print(tokenizer.batch_decode(outputs, max_new_tokens=256, skip_special_tokens=True))
