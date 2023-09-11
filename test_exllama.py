ea135e56321010881bb73fdf3abaccd1232f3744
Yerong <yerong.li@outlook.com>
2023-09-11 07:00:06 -0500

    test exllama

--- test_exllama.py
+++ test_exllama.py
@@ -1,18 +1,154 @@
-import torch
 from model import ExLlama, ExLlamaCache, ExLlamaConfig
 from tokenizer import ExLlamaTokenizer
 from generator import ExLlamaGenerator
+from lora import ExLlamaLora
+import perplexity
+from perplexity import Perplexity
+import time
+import torch
+import torch.nn.functional as F
+import argparse
+import json
+import math
+import sys
+import os
+import glob
+import model_init
 
-# Define the folder path where the model is saved
-model_folder = "/scratch/yerong/.cache/pyllama/Llama-2-7B-GPTQ"
+torch.cuda._lazy_init()
+# torch.backends.cuda.matmul.allow_tf32 = True
+# torch.backends.cuda.matmul.allow_fp16_reduced_precision_reduction = True
+torch.set_printoptions(precision = 10)
+torch_devices = [f"cuda:{i}" for i in range(torch.cuda.device_count())]
 
-# Load the model and tokenizer
-model = ExLlama.load_from_folder(model_folder)
-tokenizer = ExLlamaTokenizer.from_folder(model_folder)
+cache = None
+model = None
 
-# Create a cache for the model
+def begin():
+    global model, cache
+
+    if cache is None: cache = ExLlamaCache(model)
+    else: cache.current_seq_len = 0
+
+
+def next_logits(input_ids, apply_lora, last_id_only = True, input_mask = None):
+    global model, cache
+
+    # n_logits = None
+    # a = 0
+    # while a < input_ids.shape[-1]:
+    #     b = min(input_ids.shape[-1], a + 2048)
+    #     n_logits = model.forward(input_ids[:, a:b], cache, last_id_only, lora = apply_lora, input_mask = input_mask)
+    #     a = b
+
+    n_logits = model.forward(input_ids, cache, last_id_only, lora=apply_lora, input_mask=input_mask)
+    return n_logits
+
+
+def tokenize(text):
+    global tokenizer
+
+    return tokenizer.encode(text)
+
+
+def timer(name, func):
+    t = time.time()
+    ret = func()
+    t = time.time() - t
+    print(f" ** Time, {name}: {t:.2f} seconds")
+    return ret
+
+
+mem_base = {}
+mem_last = {}
+for dev in torch_devices:
+    torch.cuda.reset_peak_memory_stats(dev)
+    mem_base[dev] = mem_last[dev] = torch.cuda.max_memory_allocated(dev)
+
+def mem(name, total = False):
+    global mem_base, mem_last
+
+    res = f" ** VRAM, {name}: "
+    first = True
+
+    for device in torch_devices:
+        mem_c = torch.cuda.max_memory_allocated(device)
+        mem_this = mem_c - mem_last[device] if not total else mem_c - mem_base[device]
+        mem_last[device] = mem_c
+
+        if not first: res += " - "
+        first = False
+        res += f"[{device}] {mem_this / (1024 ** 2):,.2f} MB"
+
+    print(res)
+
+
+# Parse arguments
+
+parser = argparse.ArgumentParser(description = "Benchmark tests for ExLlama")
+
+model_init.add_args(parser)
+perplexity.add_args(parser)
+
+parser.add_argument("-p", "--perf", action = "store_true", help = "Benchmark speed and VRAM usage")
+parser.add_argument("-v", "--validate", action = "count", help = "Run validation check and generate some sample output; specify twice for a more thorough test")
+parser.add_argument("-lora", "--lora", type = str, help = "Path to LoRA binary to use during benchmark")
+parser.add_argument("-loracfg", "--lora_config", type = str, help = "Path to LoRA config to use during benchmark")
+parser.add_argument("-ld", "--lora_dir", type = str, help = "Path to LoRA config and binary. to use during benchmark")
+
+args = parser.parse_args()
+
+model_init.post_parse(args)
+perplexity.post_parse(args)
+model_init.get_model_files(args)
+
+# Paths
+
+if args.lora_dir is not None:
+    args.lora_config = os.path.join(args.lora_dir, "adapter_config.json")
+    args.lora = os.path.join(args.lora_dir, "adapter_model.bin")
+
+# Feedback
+
+print_opts = []
+if args.perf: print_opts.append("perf")
+if args.validate: print_opts.append("validate")
+if args.perplexity: print_opts.append("perplexity")
+if args.perplexity_token: print_opts.append("perplexity_token")
+
+model_init.print_options(args, print_opts)
+
+# Globals
+
+model_init.set_globals(args)
+
+# Instantiate model
+
+config = model_init.make_config(args)
+
+model = timer("Load model", lambda: ExLlama(config))
+tokenizer = timer("Load tokenizer", lambda: ExLlamaTokenizer(args.tokenizer))
+
+model_init.print_stats(model)
+
+torch.cuda.reset_peak_memory_stats("cuda")
+mem("Model")
+
 cache = ExLlamaCache(model)
+mem("Cache")
 
+# Load LoRA
+
+lora = None
+if args.lora:
+    print(f" -- LoRA config: {args.lora_config}")
+    print(f" -- Loading LoRA: {args.lora}")
+    if args.lora_config is None:
+        print(f" ## Error: please specify lora path to adapter_config.json")
+        sys.exit()
+    lora = ExLlamaLora(model, args.lora_config, args.lora)
+    if lora.bias_ignored:
+        print(f" !! Warning: LoRA zero bias ignored")
 # Define your prompt
 prompt = "who is elon musk"
 
