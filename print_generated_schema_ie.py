1e0e3daa49126f97daf198bd5881cfa9b34e2a62
Yerong <yerong.li@outlook.com>
2023-09-18 08:58:30 -0500

    Print schema

--- print_generated_schema_ie.py
+++ print_generated_schema_ie.py
@@ -9,7 +9,7 @@ def print_instructions_from_jsonl(file_path):
         with open(file_path, 'r', encoding='utf-8') as file:
             for line in file:
                 data = json.loads(line)
-                instruction = data.get("instruction")
+                instruction = data.get("instance")
                 if instruction:
                     print(instruction)
     except FileNotFoundError:
