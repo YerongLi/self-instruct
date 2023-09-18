d73ff701350437a5a19fe80fd89a1bd1454d0ac8
Yerong <yerong.li@outlook.com>
2023-09-18 08:18:40 -0500

    Print generated instructions.py

---
+++ print_generated_instructions_ie.py
@@ -0,0 +1,21 @@
+import json
+
+# Define the path to the JSONL file
+file_path = 'data/ie/machine_generated_instructions.jsonl'
+
+# Function to read and print "instruction" fields from the JSONL file
+def print_instructions_from_jsonl(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            for line in file:
+                data = json.loads(line)
+                instruction = data.get("instruction")
+                if instruction:
+                    print(instruction)
+    except FileNotFoundError:
+        print(f"File '{file_path}' not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+# Call the function to print instructions
+print_instructions_from_jsonl(file_path)
