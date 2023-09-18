737dbff2ee3bca2c017e31ad859bb81a7089717b
Yerong <yerong.li@outlook.com>
2023-09-18 08:08:29 -0500

    Return a string

--- scripts/generate_instances_ie.sh
+++ scripts/generate_instances_ie.sh
@@ -8,7 +8,7 @@ python self_instruct/generate_instances_ie.py \
     --batch_dir ${batch_dir} \
     --input_file machine_generated_instructions.jsonl \
     --output_file machine_generated_instances.jsonl \
-    --max_instances_to_gen 5 \
+    --max_instances_to_gen 30 \
     --engine "davinci" \
     --request_batch_size 1 \
     ${test_flag}
