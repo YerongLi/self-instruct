f89f53d09e09282e2f0d1fe21e755e8ade5d60e4
Yerong <yerong.li@outlook.com>
2023-09-18 08:07:45 -0500

    Return a string

--- scripts/generate_instructions.sh
+++ scripts/generate_instructions.sh
@@ -2,7 +2,7 @@ batch_dir=data/gpt3_generations/
 
 python self_instruct/bootstrap_instructions.py \
     --batch_dir ${batch_dir} \
-    --num_instructions_to_generate 5 \
+    --num_instructions_to_generate 30 \
     --seed_tasks_path data/seed_tasks.jsonl \
     --request_batch_size 1 \
     --engine "davinci"
