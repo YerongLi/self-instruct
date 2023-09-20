67d58b2d4da04659297d1acf428632f85a228db5
Yerong <yerong.li@outlook.com>
2023-09-20 03:51:56 -0500

    instance_ie to format_ie

--- scripts/generate_format_ie.sh
+++ scripts/generate_format_ie.sh
@@ -1,7 +1,7 @@
 batch_dir=data/ie/
 
-rm ${batch_dir}/machine_generated_instances.jsonl
-python self_instruct/generate_instances_ie.py \
+rm ${batch_dir}/machine_generated_format.jsonl
+python self_instruct/generate_format_ie.py \
     --batch_dir ${batch_dir} \
     --input_file machine_generated_instructions.jsonl \
     --output_file machine_generated_instances.jsonl \
