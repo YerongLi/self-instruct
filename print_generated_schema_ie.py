215f92e86a6e886b37972975144a4d66b473f2ff
Yerong <yerong.li@outlook.com>
2023-09-20 04:49:18 -0500

    Print the clean_format only

--- print_generated_schema_ie.py
+++ print_generated_schema_ie.py
@@ -1,7 +1,20 @@
 import json
+def extract_prefix_until_index(input_string):
+    lines = input_string.split('\n')  # Split the string into lines
+
+    output = []
+
+    for line in lines:
+        # Use regular expression to check if the line starts with an index
+        if re.match(r'^\d+\.\s', line):
+            break
+        output.append(line)
+
+    result = '\n'.join(output)
+    return result
 
 # Define the path to the JSONL file
-file_path = 'data/ie/machine_generated_instances.jsonl'
+file_path = 'data/ie/machine_generated_format.jsonl'
 
 # Function to read and print "instruction" fields from the JSONL file
 def print_instructions_from_jsonl(file_path):
@@ -10,10 +23,11 @@ def print_instructions_from_jsonl(file_path):
             for line in file:
                 data = json.loads(line)
                 instruction = data.get("instruction")
-                schema = data.get("raw_instances")
+                raw_format = data.get("raw_instances")
+                clean_format = extract_prefix_until_index(raw_format)
                 if instruction:
                     print(f"Instruction\n {instruction.rstrip()}")
-                    print(f"Format\n {schema}")
+                    print(f"Format\n {clean_format}")
                     print("===")
     except FileNotFoundError:
         print(f"File '{file_path}' not found.")
