c2c03073a0832dafa47f29db0ed93d1b36d3cc2d
Yerong <yerong.li@outlook.com>
2023-09-20 04:14:55 -0500

    instance_ie to format_ie

--- scripts/generate_format_ie.sh
+++ scripts/generate_format_ie.sh
@@ -4,7 +4,7 @@ rm ${batch_dir}/machine_generated_format.jsonl
 python self_instruct/generate_format_ie.py \
     --batch_dir ${batch_dir} \
     --input_file machine_generated_instructions.jsonl \
-    --output_file machine_generated_instances.jsonl \
+    --output_file machine_generated_format.jsonl \
     --max_instances_to_gen 30 \
     --engine "davinci" \
     --request_batch_size 1 \
