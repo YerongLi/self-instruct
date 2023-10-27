2c67262191bff25405d92d0bf4492c9fcaf6eb4c
Yerong <yerong.li@outlook.com>
2023-10-27 09:38:31 -0500

    Errors

--- tax/xpo_view.py
+++ tax/xpo_view.py
@@ -1,18 +1,27 @@
 import json
-
+import sys
 # Load the JSON file
 with open('xpo_v1.1.json', 'r') as json_file:
     data = json.load(json_file)
 
 # Iterate through the data and print the "name" values under "overlay_parents"
-for key, value in data.items():
-    print(value['name'])
+
+# Assuming 'data' is a dictionary containing the data
+keys = sorted(data.keys(), key=lambda x: x[::-1])
+
+# Iterate over the keys and print the iteration number and corresponding values
+for i, key in enumerate(keys):
+    value = data[key]
+    print(f"Iteration {i+1}\n Name: {value['name']}")
+
     if 'overlay_parents' in value:
         overlay_parents = value['overlay_parents']
         for overlay_parent in overlay_parents:
-            # if 'name' in overlay_parent:
+            if 'name' in overlay_parent:
                 print(f"   Overlay Parent name: {overlay_parent['name']}")
+
     print()  # Print a newline after each entry
 
-
-print(len(data))
+    # Check if it's time to pause and wait for the enter key
+    if (i + 1) % 5 == 0:
+        sys.stdin.readline()  # Wait for the enter key
