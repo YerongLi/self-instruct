3ca4ad1d3faaa75ffeb7a64f30fc2d1f29f33d77
Yerong <yerong.li@outlook.com>
2023-11-09 11:55:53 -0600

    Upload the video documents

--- pipe/pipe.py
+++ pipe/pipe.py
@@ -1,30 +1,52 @@
 import mediapipe as mp
-from mediapipe.tasks import python
-from mediapipe.tasks.python import vision
-
-import mediapipe as mp
+import cv2
 
 BaseOptions = mp.tasks.BaseOptions
-FaceDetector = mp.tasks.vision.FaceDetector
-FaceDetectorOptions = mp.tasks.vision.FaceDetectorOptions
-VisionRunningMode = mp.tasks.vision.RunningMode
+FaceDetector = mp.solutions.face_detection.FaceDetection
+FaceDetectorOptions = mp.solutions.face_detection.FaceDetectionOptions
+VisionRunningMode = mp.solutions.face_detection.FaceDetectionRunningMode
 
-# Create a face detector instance with the video mode:
-options = FaceDetectorOptions(
-    base_options=BaseOptions(model_asset_path='/path/to/model.task'),
-    running_mode=VisionRunningMode.VIDEO)
-with FaceDetector.create_from_options(options) as detector:
-  # The detector is initialized. Use it here.
-  # ..
-  import mediapipe as mp
+def process_video(input_video_path):
+    # Create a face detector instance with the video mode
+    options = FaceDetectorOptions(
+        base_options=BaseOptions(model_asset_path='/path/to/model.task'),
+        running_mode=VisionRunningMode.VIDEO
+    )
+    with FaceDetector(options) as detector:
+        cap = cv2.VideoCapture(input_video_path)
 
-# Use OpenCV’s VideoCapture to load the input video.
+        frame_count = 0
+        while cap.isOpened():
+            ret, frame = cap.read()
 
-# Load the frame rate of the video using OpenCV’s CV_CAP_PROP_FPS
-# You’ll need it to calculate the timestamp for each frame.
+            if not ret:
+                break
 
-# Loop through each frame in the video using VideoCapture#read()
+            frame_count += 1
+            print(f"Processing frame {frame_count}")
 
-	# Convert the frame received from OpenCV to a MediaPipe’s Image object.
-	mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=numpy_frame_from_opencv)
-    
+            # Convert frame to RGB (MediaPipe requires RGB input)
+            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            # Convert the frame to MediaPipe's Image object
+            mp_image = mp.solutions.drawing_utils._image_from_image(frame_rgb)
+
+            # Get timestamp for the frame (in milliseconds)
+            frame_timestamp_ms = cap.get(cv2.CAP_PROP_POS_MSEC)
+
+            # Perform face detection on the provided single image
+            face_detector_result = detector.process(mp_image, frame_timestamp_ms)
+
+            # Perform operations with the face detector results (e.g., drawing bounding boxes)
+            # For example:
+            for detection in face_detector_result.detections:
+                # Retrieve and process the face detection data
+
+            # Show or save annotated frames
+            # ...
+
+        cap.release()
+
+# Process the video
+input_video_path = 'your_video.mp4'
+process_video(input_video_path)
