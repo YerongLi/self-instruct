30b6f5834418e3ce51d9a99d451dbced3590e535
Yerong <yerong.li@outlook.com>
2023-11-09 11:55:19 -0600

    Upload the video documents

--- pipe/pipe.py
+++ pipe/pipe.py
@@ -1,91 +1,30 @@
-# https://github.com/google/mediapipe.git
-# !pip install -q mediapipe==0.10.0
-# !wget -q -O efficientdet.tflite -q https://storage.googleapis.com/mediapipe-models/object_detector/efficientdet_lite0/int8/1/efficientdet_lite0.tflite
-#@markdown We implemented some functions to visualize the object detection results. <br/> Run the following cell to activate the functions.
-import cv2
 import mediapipe as mp
-import numpy as np
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 
-MARGIN = 10  # pixels
-ROW_SIZE = 10  # pixels
-FONT_SIZE = 1
-FONT_THICKNESS = 1
-TEXT_COLOR = (255, 0, 0)  # red
+import mediapipe as mp
 
+BaseOptions = mp.tasks.BaseOptions
+FaceDetector = mp.tasks.vision.FaceDetector
+FaceDetectorOptions = mp.tasks.vision.FaceDetectorOptions
+VisionRunningMode = mp.tasks.vision.RunningMode
 
-def visualize(
-    image,
-    detection_result
-) -> np.ndarray:
-  """Draws bounding boxes on the input image and return it.
-  Args:
-    image: The input RGB image.
-    detection_result: The list of all "Detection" entities to be visualize.
-  Returns:
-    Image with bounding boxes.
-  """
-  for detection in detection_result.detections:
-    # Draw bounding_box
-    bbox = detection.bounding_box
-    start_point = bbox.origin_x, bbox.origin_y
-    end_point = bbox.origin_x + bbox.width, bbox.origin_y + bbox.height
-    cv2.rectangle(image, start_point, end_point, TEXT_COLOR, 3)
+# Create a face detector instance with the video mode:
+options = FaceDetectorOptions(
+    base_options=BaseOptions(model_asset_path='/path/to/model.task'),
+    running_mode=VisionRunningMode.VIDEO)
+with FaceDetector.create_from_options(options) as detector:
+  # The detector is initialized. Use it here.
+  # ..
+  import mediapipe as mp
 
-    # Draw label and score
-    category = detection.categories[0]
-    category_name = category.category_name
-    probability = round(category.score, 2)
-    result_text = category_name + ' (' + str(probability) + ')'
-    text_location = (MARGIN + bbox.origin_x,
-                     MARGIN + ROW_SIZE + bbox.origin_y)
-    cv2.putText(image, result_text, text_location, cv2.FONT_HERSHEY_PLAIN,
-                FONT_SIZE, TEXT_COLOR, FONT_THICKNESS)
+# Use OpenCV’s VideoCapture to load the input video.
 
-  return image
-# !wget -q -O image.jpg https://storage.googleapis.com/mediapipe-tasks/object_detector/cat_and_dog.jpg
+# Load the frame rate of the video using OpenCV’s CV_CAP_PROP_FPS
+# You’ll need it to calculate the timestamp for each frame.
 
-def process_video(input_video_path, output_path):
-    # STEP 2: Create an ObjectDetector object.
-    base_options = python.BaseOptions(model_asset_path='efficientdet.tflite')
-    options = vision.ObjectDetectorOptions(base_options=base_options, score_threshold=0.5)
-    detector = vision.ObjectDetector.create_from_options(options)
+# Loop through each frame in the video using VideoCapture#read()
 
-    # Read video file
-    cap = cv2.VideoCapture(input_video_path)
-    frame_count = 0
-
-    while cap.isOpened():
-        ret, frame = cap.read()
-
-        if not ret:
-            break
-
-        frame_count += 1
-        print(f"Processing frame {frame_count}")
-
-        # Convert frame to RGB
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-        # Create MediaPipe Image
-        image = mp.Image(
-            data=frame_rgb
-        )
-
-        # Detect objects in the frame.
-        detection_result = detector.detect(image)
-
-        # Process the detection result and save the annotated frame.
-        annotated_frame = visualize(frame, detection_result)
-        output_file_path = f"{output_path}/frame_{frame_count:04d}.png"
-        cv2.imwrite(output_file_path, annotated_frame)
-
-    cap.release()
-
-
-
-# Process the video and save annotated frames
-input_video_path = 'Phase II Lesson 1 Elaboration Identifying Additional Element.mp4'
-output_directory = 'annotated_frames'
-process_video(input_video_path, output_directory)
+	# Convert the frame received from OpenCV to a MediaPipe’s Image object.
+	mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=numpy_frame_from_opencv)
+    
