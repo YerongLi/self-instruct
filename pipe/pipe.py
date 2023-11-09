27fc64a121499eb142986c2a8c5ee2a47c20f88e
Yerong <yerong.li@outlook.com>
2023-11-09 11:25:28 -0600

    Use == instead of =

--- pipe/pipe.py
+++ pipe/pipe.py
@@ -46,7 +46,7 @@ def visualize(
 IMAGE_FILE = 'image.jpg'
 
 import cv2
-from google.colab.patches import cv2_imshow
+# from google.colab.patches import cv2_imshow
 
 img = cv2.imread(IMAGE_FILE)
-cv2_imshow(img)
+# cv2_imshow(img)
