5e7831b6e636afc5cc88978d645d528c694d3508
Yerong <yerong.li@outlook.com>
2023-11-09 22:02:29 -0600

    --save-predictions

--- pipe/mmpose-main/run.sh
+++ pipe/mmpose-main/run.sh
@@ -4,4 +4,4 @@ python demo/topdown_demo_with_mmdet.py \
     configs/face_2d_keypoint/rtmpose/face6/rtmpose-m_8xb256-120e_face6-256x256.py \
     https://download.openmmlab.com/mmpose/v1/projects/rtmposev1/rtmpose-m_simcc-face6_pt-in1k_120e-256x256-72a37400_20230529.pth \
     --input "Phase II Lesson 1 Elaboration Identifying Additional Element.mp4" \
-    --output-root vis_results --radius 1
+    --output-root vis_results --radius 1 --save-predictions
