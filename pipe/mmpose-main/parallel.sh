162625c506acd600740919ccc50e0077648ce1e2
Yerong <yerong.li@outlook.com>
2023-11-10 01:40:32 -0600

    Update

--- pipe/mmpose-main/parallel.sh
+++ pipe/mmpose-main/parallel.sh
@@ -1,14 +1,3 @@
-# find "Demonstration Videos" -name "*.mp4" -print0 | xargs -0 -n 1 -P 12 ./run.sh
-# Set the maximum number of parallel processes
-MAX_PROCESSES=12
-COMMAND="python demo/topdown_demo_with_mmdet.py"
+task=$1
+find "Demonstration Videos" -name "*.mp4" -print0 | xargs -0 -n 1 -P 12 ./run_$task.sh
 
-find "Demonstration Videos" -name "*.mp4" | while read -r file; do
-    ./run.sh "$file" &
-
-    while [ $(pgrep "$COMMAND" | wc -l) -ge $MAX_PROCESSES ]; do
-        sleep 0.1
-    done
-done
-
-wait
