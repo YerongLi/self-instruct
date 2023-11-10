04fdac8ed27f28f258eca8878cc521d5edb9b162
Yerong <yerong.li@outlook.com>
2023-11-09 22:45:07 -0600

    Update the run.sh

--- pipe/mmpose-main/run.sh
+++ pipe/mmpose-main/run.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
+
+# Assign the video name to a variable
+video_name="$1"
+
+# Run the Python script with the provided arguments
 python demo/topdown_demo_with_mmdet.py \
     demo/mmdetection_cfg/yolox-s_8xb8-300e_coco-face.py \
     https://download.openmmlab.com/mmpose/mmdet_pretrained/yolo-x_8xb8-300e_coco-face_13274d7c.pth \
     configs/face_2d_keypoint/rtmpose/face6/rtmpose-m_8xb256-120e_face6-256x256.py \
     https://download.openmmlab.com/mmpose/v1/projects/rtmposev1/rtmpose-m_simcc-face6_pt-in1k_120e-256x256-72a37400_20230529.pth \
-    --input "Phase II Lesson 1 Elaboration Identifying Additional Element.mp4" \
+    --input "$video_name" \
     --output-root vis_results --radius 1 --save-predictions
