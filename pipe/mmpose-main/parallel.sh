e6f7ff463980d937281236b5ce0638662a557174
Yerong <yerong.li@outlook.com>
2023-11-09 23:03:35 -0600

    Upload

--- pipe/mmpose-main/parallel.sh
+++ pipe/mmpose-main/parallel.sh
@@ -1,4 +1,4 @@
 cd "Demonstration Videos"
 
 # Step 5: Iterate over each .mp4 file and run the command
-find . -name "*.mp4" -print0 | xargs -0 -n 1 -P 4 ./run.sh
+find . -name "*.mp4" -print0 | xargs -0 -n 1 -P 8 ./run.sh
