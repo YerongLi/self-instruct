36d959667043a04d1b5414285e743694741ce8cd
Yerong <yerong.li@outlook.com>
2023-11-10 19:32:32 -0600

    Upload the count.py

--- pipe/count.py
+++ pipe/count.py
@@ -1,12 +1,23 @@
 import cv2
+import os
 
+video_dir = "mmpose-main/Demonstration Videos"
+video_files = []
+
+# List all files in the directory
+for file in os.listdir(video_dir):
+    # Check if the file is a video file (e.g., .mp4, .avi, .mov, etc.)
+    if file.endswith(".mp4") or file.endswith(".avi") or file.endswith(".mov"):
+        video_files.append(os.path.join(video_dir, file))
+
+# Print the list of video files
+for video_file in video_files:
+    print(video_file)
+    frame_count = count_frames(video_file)
+    print("Total frames:", frame_count)
 def count_frames(input_video_path):
     cap = cv2.VideoCapture(input_video_path)
     total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     cap.release()
     return total_frames
 
-# Usage
-input_video_path = 'mmpose-main/Demonstration Videos/Phase II Lesson 1 Elaboration Identifying Additional Element.mp4'
-frame_count = count_frames(input_video_path)
-print("Total frames:", frame_count)
