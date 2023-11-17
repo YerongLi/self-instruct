9cbb96fa5aa9b9a811b43d705e66e35a704aee8c
Yerong <yerong.li@outlook.com>
2023-11-16 20:46:16 -0600

    order the suffix

--- pipe/pdf.py
+++ pipe/pdf.py
@@ -28,7 +28,7 @@ def create_pdf_with_rescaled_pair(folder_path, output_pdf):
         if not image_file.lower().endswith('.jpg'): continue
         # Calculate the width and height of the image (optional)
         # print(image_file)
-        base_filename = image_file[:-3]
+        base_filename = image_file[:-4]
         img_path = os.path.join(folder_path, image_file)
         img = utils.ImageReader(img_path)
         img_width, img_height = img.getSize()
