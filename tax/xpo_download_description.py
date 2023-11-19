c34d9a7eceb82286e59ff9e4f708a229fc0bc760
Yerong <yerong.li@outlook.com>
2023-11-19 02:31:15 -0600

    Initialize script for downloading the description of all parents.py

--- tax/xpo_download_description.py
+++ tax/xpo_download_description.py
@@ -10,6 +10,37 @@ with open('xpo_v1.1.json', 'r') as json_file:
 keys = sorted(data.keys(), key=lambda x: x[::-1])
 start_index = -1
 # Iterate over the keys and print the iteration number and corresponding values
+def get_wikidata_description(wikidata_id):
+    # Wikidata API endpoint
+    api_endpoint = "https://www.wikidata.org/w/api.php"
+
+    # Parameters for the API request
+    params = {
+        'action': 'wbgetentities',
+        'ids': wikidata_id,
+        'format': 'json',
+        'props': 'descriptions',
+        'languages': 'en',  # Change 'en' to the desired language code
+    }
+
+    try:
+        # Make the API request
+        response = requests.get(api_endpoint, params=params)
+        response.raise_for_status()  # Check for errors in the HTTP response
+
+        # Parse the JSON response
+        data = response.json()
+
+        # Get the description from the response
+        description = data['entities'][wikidata_id]['descriptions']['en']['value']
+
+        return description
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error making the request: {e}")
+        return None
+
+ans = {}
 for i, key in enumerate(keys):
     if i <= start_index : continue
     value = data[key]
@@ -18,10 +49,13 @@ for i, key in enumerate(keys):
     if 'overlay_parents' in value:
         overlay_parents = value['overlay_parents']
         for overlay_parent in overlay_parents:
-            print(overlay_parents)
-            # if 'name' in overlay_parent:
+            # print(overlay_parents)
+            if 'wd_node' in overlay_parent and overlay_parent['wd_node'] not in ans:
+                ans[overlay_parent['wd_node']] = get_wikidata_description(overlay_parent['wd_node'])
                 # print(f"   Overlay Parent name: {overlay_parent['name']}")
 
     print()  # Print a newline after each entry
+random_entries = random.sample(list(ans.keys()), 5)
 
-    # Check if it's time to pause and wait for the enter key
+for key in random_entries:
+    print(key, ans[key])
