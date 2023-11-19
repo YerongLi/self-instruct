1ddfcf64d3c0dc7f37307d8fcd096d3a2df2b4d0
Yerong <yerong.li@outlook.com>
2023-11-19 02:25:56 -0600

    Initialize script for downloading the description of all parents.py

---
+++ tax/xpo_download_description.py
@@ -0,0 +1,27 @@
+import json
+import sys
+# Load the JSON file
+with open('xpo_v1.1.json', 'r') as json_file:
+    data = json.load(json_file)
+
+# Iterate through the data and print the "name" values under "overlay_parents"
+
+# Assuming 'data' is a dictionary containing the data
+keys = sorted(data.keys(), key=lambda x: x[::-1])
+start_index = -1
+# Iterate over the keys and print the iteration number and corresponding values
+for i, key in enumerate(keys):
+    if i <= start_index : continue
+    value = data[key]
+    print(f"Iteration {i+1}\n Name: {value['name']}")
+
+    if 'overlay_parents' in value:
+        overlay_parents = value['overlay_parents']
+        for overlay_parent in overlay_parents:
+            print(overlay_parents.keys())
+            if 'name' in overlay_parent:
+                # print(f"   Overlay Parent name: {overlay_parent['name']}")
+
+    print()  # Print a newline after each entry
+
+    # Check if it's time to pause and wait for the enter key
