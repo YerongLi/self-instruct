a4e4b6ed5bea0e08449f99b3af6059d65e4d3d0c
Yerong <yerong.li@outlook.com>
2023-11-26 03:14:53 -0600

    definitions.pkl

--- tax/query.py
+++ tax/query.py
@@ -20,15 +20,28 @@ with open('../../TaxoComplete/definitions.pkl', 'rb') as f:
 
 logging.info(core_graph)
 ans = -0x7f7f7f7f
+single_neighbor_count = 0
+
 # print(definitions)
 max_node = None
 for node in core_graph.nodes():
     if definitions[node]['label'] == ' ': continue
     if not core_graph.has_node(node): continue
+
     length = len([ _ for _ in core_graph.neighbors(node)])
+
+    if length == 1:
+        single_neighbor_count += 1
+
     if length > ans:
         ans = length
         max_node = node
-print(definitions[max_node])
+
+# print(definitions[max_node])
 # print(core_graph.neighbors(max_node))
 logging.info(f"Max number of the neighbourhoods are {ans}")
+logging.info(f"Number of nodes with only one neighbor: {single_neighbor_count}")
+
+for edge in core_graph.edges():
+    parent, kid = edge
+    weight = core_graph[parent][kid]
