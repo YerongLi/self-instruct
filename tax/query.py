cbba35427acd46e0870476117b68e5e43e81ee10
Yerong <yerong.li@outlook.com>
2023-11-26 03:26:50 -0600

    definitions.pkl

--- tax/query.py
+++ tax/query.py
@@ -18,9 +18,9 @@ with open('../../TaxoComplete/core_graph.pkl', 'rb') as f:
 with open('../../TaxoComplete/definitions.pkl', 'rb') as f:
     definitions = pickle.load(f)
 
-logging.info(core_graph)
 ans = -0x7f7f7f7f
 single_neighbor_count = 0
+zero_neighbor_count = 0
 
 # print(definitions)
 max_node = None
@@ -28,9 +28,11 @@ for node in core_graph.nodes():
     if definitions[node]['label'] == ' ': continue
     if not core_graph.has_node(node): continue
 
-    length = len([ _ for _ in core_graph.neighbors(node)])
+    length = len([_ for _ in core_graph.neighbors(node)])
 
-    if length == 1:
+    if length == 0:
+        zero_neighbor_count += 1
+    elif length == 1:
         single_neighbor_count += 1
 
     if length > ans:
@@ -39,12 +41,12 @@ for node in core_graph.nodes():
 
 # print(definitions[max_node])
 # print(core_graph.neighbors(max_node))
-logging.info(f"Max number of the neighbours are {ans}")
+logging.info(f"Max number of the neighbors are {ans}")
+logging.info(f"Number of nodes with zero neighbors: {zero_neighbor_count}")
 logging.info(f"Number of nodes with only one neighbor: {single_neighbor_count}")
-
-
 ans = -0x7f7f7f7f
 single_neighbor_count = 0
+zero_neighbor_count = 0
 
 # print(definitions)
 max_node = None
@@ -52,11 +54,13 @@ for node in core_graph.nodes():
     if definitions[node]['label'] == ' ': continue
     if not core_graph.has_node(node): continue
 
-    length = len([ _ for _ in core_graph.predecessors(node)])
+    length = len([_ for _ in core_graph.predecessors(node)])
 
-    if length == 1:
+    if length == 0:
+        zero_neighbor_count += 1
+    elif length == 1:
         single_neighbor_count += 1
-        
+
     if length > ans:
         ans = length
         max_node = node
@@ -64,11 +68,8 @@ for node in core_graph.nodes():
 # print(definitions[max_node])
 # print(core_graph.neighbors(max_node))
 logging.info(f"Max number of the predecessors are {ans}")
+logging.info(f"Number of nodes with zero predecessors: {zero_neighbor_count}")
 logging.info(f"Number of nodes with only one predecessor: {single_neighbor_count}")
-
-for edge in core_graph.edges():
-    parent, kid = edge
-    weight = core_graph[parent][kid]
 
 
 for edge in core_graph.edges():
