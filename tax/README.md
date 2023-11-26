bb2d8ea0fa770ae1fbaa409780e38d4c87815adb
Yerong <yerong.li@outlook.com>
2023-11-26 02:28:16 -0600

    Query

--- tax/README.md
+++ tax/README.md
@@ -1,1 +1,3 @@
 perturbate.py is the file that does perturbation on the original taxo
+##
+query.py probe the LLM for question
---
+++ tax/query.py
@@ -0,0 +1,22 @@
+import logging
+import os
+import pickle
+import random
+import tqdm
+TOTAL = 300
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-4s - %(filename)-6s:%(lineno)d - %(message)s',
+    level=logging.INFO,
+    filename='./output.log',
+    datefmt='%m-%d %H:%M:%S')
+
+logging.info(f'Logger start: {os.uname()[1]}')
+
+# Load the definitions variable from the file
+with open('../../TaxoComplete/core_graph.pkl', 'rb') as f:
+    core_graph = pickle.load(f)
+with open('../../TaxoComplete/data_prep.pkl', 'rb') as f:
+    data_prep = pickle.load(f)
+
+logging.info(core_graph)
+logging.info(data_prep)
