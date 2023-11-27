17e5c50514e5146e4a0bd655c9a55d994c2227c2
Yerong <yerong.li@outlook.com>
2023-11-27 05:46:54 -0600

    700

--- tax/query1.py
+++ tax/query1.py
@@ -19,12 +19,12 @@ logging.info(f'Logger start: {os.uname()[1]}')
 
 model_path = "/scratch/yerong/.cache/pyllama/Llama-2-7b-hf/"
 
-model = None
-# model = LlamaForCausalLM.from_pretrained(
-#   model_path,
-#   torch_dtype=torch.float16,
-#   device_map='auto',
-# ).eval()
+# model = None
+model = LlamaForCausalLM.from_pretrained(
+  model_path,
+  torch_dtype=torch.float16,
+  device_map='auto',
+).eval()
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 tokenizer.pad_token = tokenizer.eos_token
 tokenizer.padding_side = "left"
