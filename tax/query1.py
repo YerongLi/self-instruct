96f22773bbd5205fac93b8e356853569155ad80d
Yerong <yerong.li@outlook.com>
2023-11-27 06:15:36 -0600

    700

--- tax/query1.py
+++ tax/query1.py
@@ -395,6 +395,8 @@ batch_size = 10
 
 
 predictions =predict_next_token_batch(prompts, batch_size)
+print(len(predictions))
+print(len(prompts))
 results = [{'label':prompts[i]['label'], 'pred': predictions[i]} for i in range(len(prompts))]
 # output_sequences = model.generate(**inputs, max_new_tokens=20, do_sample=True, top_p=0.9)
 
