ab64499293cd35481ad741e7c80dff2cd9e5ad53
Yerong <yerong.li@outlook.com>
2023-11-26 22:58:36 -0600

    Construct prompt

--- tax/query1.py
+++ tax/query1.py
@@ -145,7 +145,6 @@ for edge in tqdm.tqdm(core_graph.edges()):
 
 
     edge_list = edges_within_k_edges(core_graph, parent, kid)
-    print(len(edge_list))
     # Sample 6 edges from edge_list
     sampled_edges = random.sample(edge_list, min(6, len(edge_list)))
 
