513735a42ac4b52e4d1310da5aa688225f2a1854
Yerong <yerong.li@outlook.com>
2023-11-26 23:10:22 -0600

    Construct prompt

--- tax/query1.py
+++ tax/query1.py
@@ -182,7 +182,37 @@ for edge in tqdm.tqdm(core_graph.edges()):
     if max_pair is None or edge_list_len > max_len:
         max_pair = (parent, kid)
         max_len = edge_list_len
+    # Check if we need to sample additional negative pairs
+    if len(edge_list) < len(nodes) * (len(nodes) - 1) - 6:
+        # Sample all negative pairs within nodes
+        negative_pairs = []
+        for node1 in nodes:
+            for node2 in nodes:
+                if node1 != node2 and (node1, node2) not in edge_list and (node2, node1) not in edge_list:
+                    negative_pairs.append((node1, node2))
 
+        # Sample additional negative pairs from core_graph.nodes()
+        num_additional_pairs = 6 - (len(nodes) * (len(nodes) - 1) - len(edge_list))
+        additional_pairs = []
+        while len(additional_pairs) < num_additional_pairs:
+            node1 = random.choice(list(core_graph.nodes()))
+            node2 = random.choice(list(nodes))
+            if (node1, node2) not in edge_list and (node2, node1) not in edge_list:
+                additional_pairs.append((node1, node2))
+
+        # Combine negative pairs and additional pairs
+        negative_pairs += additional_pairs
+
+        # Create the negative samples prompt
+        prompt += "\nNegative Samples:\n\n"
+        for pair in negative_pairs:
+            parent = pair[0]
+            kid = pair[1]
+            parent_label = get_first_label_with_n(definitions[parent]['label'])
+            kid_label = get_first_label_with_n(definitions[kid]['label'])
+            prompt += f"Pair: {parent_label} -> {kid_label}\n"
+    else:
+        raise NotImplementedError("This method has not been implemented yet.")
 if min_pair is not None:
     parent, kid = min_pair
     logging.info("Minimum pair:")
