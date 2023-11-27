fa83792f942a3fbf0a6271341091afffa1287061
Yerong <yerong.li@outlook.com>
2023-11-26 21:29:08 -0600

    Rename query.py to query1.py

--- tax/query1.py
+++ tax/query1.py
@@ -3,6 +3,8 @@ import os
 import pickle
 import random
 import tqdm
+import networkx as nx
+
 TOTAL = 300
 logging.basicConfig(
     format='%(asctime)s %(levelname)-4s - %(filename)-6s:%(lineno)d - %(message)s',
@@ -11,6 +13,43 @@ logging.basicConfig(
     datefmt='%m-%d %H:%M:%S')
 
 logging.info(f'Logger start: {os.uname()[1]}')
+
+def edges_within_k_edges(graph, parent, child, k=2):
+    # Create a set to store the visited nodes
+    visited = set()
+    
+    # Create a list to store the edges within k edge distances
+    ans = []
+    
+    # Perform DFS on the parent node
+    for node in nx.dfs_tree(graph, parent):
+        # Mark the node as visited
+        visited.add(node)
+        
+        # Check if the node is within k edge distances of the child node
+        if nx.has_path(graph, node, child) and nx.shortest_path_length(graph, node, child) <= k:
+            # Iterate over the neighbors of the node
+            for neighbor in graph.neighbors(node):
+                # Check if the neighbor is not visited and is not the parent node
+                if neighbor not in visited and neighbor != parent:
+                    # Add the edge to the list of edges within k edge distances
+                    ans.append((node, neighbor))
+    
+    # Perform DFS on the child node
+    for node in nx.dfs_tree(graph, child):
+        # Mark the node as visited
+        visited.add(node)
+        
+        # Check if the node is within k edge distances of the parent node
+        if nx.has_path(graph, node, parent) and nx.shortest_path_length(graph, node, parent) <= k:
+            # Iterate over the neighbors of the node
+            for neighbor in graph.neighbors(node):
+                # Check if the neighbor is not visited and is not the child node
+                if neighbor not in visited and neighbor != child:
+                    # Add the edge to the list of edges within k edge distances
+                    ans.append((node, neighbor))
+    
+    return ans
 
 # Load the definitions variable from the file
 with open('../../TaxoComplete/core_graph.pkl', 'rb') as f:
@@ -80,14 +119,15 @@ logging.info(f"Number of nodes with zero predecessors: {zero_neighbor_count}")
 logging.info(f"Number of nodes with one predecessor: {single_neighbor_count}")
 logging.info(f"Number of nodes with two or more predecessors: {multiple_neighbor_count}")
 
-
+# Get the statistics
 for edge in core_graph.edges():
     parent, kid = edge
-    try:
-        weight = core_graph[parent][kid]['weight']
-        if weight == -1:
-            print(parent, kid)
-    except:
-        logging.info(definitions[parent])
-        logging.info(definitions[kid])
-        logging.info(core_graph[parent][kid])
+    edges_within_k_edges(core_graph, parent, kid)
+#     try:
+#         weight = core_graph[parent][kid]['weight']
+#         if weight == -1:
+#             print(parent, kid)
+#     except:
+#         logging.info(definitions[parent])
+#         logging.info(definitions[kid])
+#         logging.info(core_graph[parent][kid])
