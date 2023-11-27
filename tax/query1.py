9e3b84f27ab46b23f496e54d54dedcbdbf7975d6
Yerong <yerong.li@outlook.com>
2023-11-27 02:13:06 -0600

    Sample neighours

--- tax/query1.py
+++ tax/query1.py
@@ -40,7 +40,7 @@ def predict_next_token(prompt):
     # print(logits[tokenizer.convert_tokens_to_ids(["Yes"])])
     next_tokens_scores = logits_processor(input_ids, logits)
     next_tokens = torch.argmax(next_tokens_scores, dim=-1)
-    print(next_tokens_scores.shape)
+    # print(next_tokens_scores.shape)
     # Calculate the probability for "No"
     yes_prob = next_tokens_scores[0][tokenizer.convert_tokens_to_ids(["Yes"])].item()
 
@@ -55,6 +55,7 @@ def predict_next_token(prompt):
     # else:
     #     prediction = -1
     logging.info('next_token')
+    logging.info(next_token)
     return  tokenizer.decode(next_tokens)
 
 
