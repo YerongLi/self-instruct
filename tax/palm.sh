4a6e3b07b30cdae387bece0df9d0949fdae9308e
Yerong <yerong.li@outlook.com>
2023-11-29 08:54:55 -0600

    Upload curl.sh

---
+++ tax/palm.sh
@@ -0,0 +1,4 @@
+curl \
+-H 'Content-Type: application/json' \
+-d '{ "prompt": { "text": "Write a story about a magic backpack"} }' \
+"https://generativelanguage.googleapis.com/v1beta3/models/text-bison-001:generateText?key=AIzaSyCOJUBq2FLkAJsqLwlfMFzKIY1X9_SnxRY"
