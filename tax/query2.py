3b750e804703b29eb40a49d7cd49f1251d493beb
Yerong <yerong.li@outlook.com>
2023-11-30 06:07:11 -0600

    LLaMA

--- tax/query2.py
+++ tax/query2.py
@@ -444,10 +444,7 @@ def predict_llama_batch(prompts, batch_size=10):
             i_ids = tokenizer(batch_sentences, return_tensors="pt", padding=True).to(device)
 
             # Generate logits for the next token using the model
-            print(len(batch_sentences))
-            print('batch_sentences')
-            print(len(i_ids))
-            print(i_ids)
+
 
             sentence_lengths = []
 
@@ -462,17 +459,13 @@ def predict_llama_batch(prompts, batch_size=10):
             c_ids, outputs = [], []
             with torch.no_grad():
                 o_ids = model.generate(**i_ids, max_new_tokens=88, do_sample=True, top_p=0.1)
-                print(o_ids)
-                for i in range(len(o_ids)):
+                for i in range(len(batch_prompts)):
                     c_ids.append(o_ids[i][o_ids[i] != pad_token_id])
-                    print('o_ids i')
-                    print(o_ids[i])
                 # print(len(o_ids))
                 # print(o_ids)
                 
                 # print(c_ids)
                 for i in range(len(batch_prompts)):
-                    print(i)
                     outputs.append(tokenizer.decode(c_ids[i], skip_special_tokens=True))
 
                     predictions[batch_prompts[i]['hs']] = {'i' : batch_sentences[i], 'o' : outputs[i]}
