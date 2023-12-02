cacc82e36bee94b3e47fbaddaba45ca2b169b20f
Yerong <yerong.li@outlook.com>
2023-12-02 06:13:02 -0600

    Upload

--- tax/query3.py
+++ tax/query3.py
@@ -644,6 +644,7 @@ def predict_gpt_batch(prompts, batch_size=2):
             model="gpt-3.5-turbo-instruct",
             prompt=[p['prompt'] for p in batch_prompts],
             temperature=0,
+            max_tokens=200,
             top_p=1,
             frequency_penalty=0,
             presence_penalty=0
