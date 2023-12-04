0bea135b62cef071a6d323d42d644bfb8cb74bc4
Yerong <yerong.li@outlook.com>
2023-12-04 00:56:23 -0600

    refractor.py

---
+++ tax/refractor.py
@@ -0,0 +1,51 @@
+
+import argparse
+import json
+import time
+import logging
+import hashlib
+import os
+import pickle
+import random
+import shutil
+import tqdm
+# import networkx as nx
+import torch
+import requests
+import json
+import openai
+import google.generativeai as palm
+import os
+
+from transformers import LlamaForCausalLM, AutoTokenizer, LogitsProcessorList
+from torch.utils.data import DataLoader, Dataset
+from openai import OpenAI
+LOGFILE='output.log'
+palm.configure(api_key=os.environ['PALM'])
+# from transformers import LlamaForCausalLM, AutoTokenizer, LogitsProcessorList
+# from torch.utils.data import DataLoader, Dataset
+parser = argparse.ArgumentParser(description="Your script description")
+# Add the configuration file argument
+parser.add_argument("config_file", type=str, help="Path to the configuration file")
+parser.add_argument("TOTAL", type=int, default=700, nargs="?", help="Number of total items to process")
+parser.add_argument("mode", type=str, default='llama', nargs="?", help="Prediction mode")
+
+def HASH(input_string):
+    # Use SHA-256 for deterministic hashing
+    hash_object = hashlib.sha256(input_string.encode())
+    hash_value = int.from_bytes(hash_object.digest(), byteorder='big')
+
+    return str(hash_value)
+args = parser.parse_args()
+TOTAL = args.TOTAL
+
+config_file = args.config_file
+# Read the configuration file
+with open(config_file) as f:
+    config = json.load(f)
+
+# Get the configuration values
+# Extract the base filename without the ".taxo" extension
+datapath = config['taxofilename'].split('/')[:-1]
+datapath = '/'.join(datapath)
+print(datapath)
--- tax/src/train.py
+++ tax/src/train.py
@@ -4,3 +4,16 @@ import numpy as np
 from datasets import load_dataset
 from transformers import T5Tokenizer, DataCollatorForSeq2Seq
 from transformers import T5ForConditionalGeneration, Seq2SeqTrainingArguments, Seq2SeqTrainer
+
+# Load the tokenizer, model, and data collator
+MODEL_NAME = "google/flan-t5-base"
+
+tokenizer = T5Tokenizer.from_pretrained(MODEL_NAME)
+model = T5ForConditionalGeneration.from_pretrained(MODEL_NAME)
+data_collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, model=model)
+
+# Acquire the training data from Hugging Face
+DATA_NAME = "yahoo_answers_qa"
+yahoo_answers_qa = load_dataset(DATA_NAME)
+
+yahoo_answers_qa = yahoo_answers_qa["train"].train_test_split(test_size=0.3)
