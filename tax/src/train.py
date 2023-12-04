1c2ce1ffce7212af2ca0d75b584575b5ee6a78df
Yerong <yerong.li@outlook.com>
2023-12-04 02:48:26 -0600

    Training workflow

--- tax/src/train.py
+++ tax/src/train.py
@@ -8,7 +8,7 @@ model_id = "google/flan-t5-base"
 
 # load model from the hub
 model = AutoModelForSeq2SeqLM.from_pretrained(model_id, device_map="auto")
-from peft import LoraConfig, get_peft_model, prepare_model_for_int8_training, TaskType
+from peft import LoraConfig, get_peft_model, TaskType
 
 # Define LoRA Config
 lora_config = LoraConfig(
