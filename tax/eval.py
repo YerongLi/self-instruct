e726373ab8b452f3b41db61ebeb01c26285d27f4
Yerong <yerong.li@outlook.com>
2023-12-04 06:27:58 -0600

    Evaluation script

--- tax/eval.py
+++ tax/eval.py
@@ -100,7 +100,7 @@ def predict_next_token_batch(prompts, batch_size=10):
 
         # Generate logits for the next token using the model
         with torch.no_grad():
-            outputs = model(**input_ids)
+            outputs = model(**(input_ids.input_ids))
             logits = outputs.logits[:, -1, :]
 
         # Process logits or do whatever you need with them
