d3c172b33bd12e5a88b52765b897fe719fc7a6a7
Yerong <yerong.li@outlook.com>
2023-12-04 00:50:32 -0600

    Begin train T5

---
+++ tax/src/train.py
@@ -0,0 +1,6 @@
+import nltk
+import evaluate
+import numpy as np
+from datasets import load_dataset
+from transformers import T5Tokenizer, DataCollatorForSeq2Seq
+from transformers import T5ForConditionalGeneration, Seq2SeqTrainingArguments, Seq2SeqTrainer
