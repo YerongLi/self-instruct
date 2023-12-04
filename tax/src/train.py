6210c2b70948d1492501b62685e9a5c73347d3e9
Yerong <yerong.li@outlook.com>
2023-12-04 02:58:16 -0600

    Training workflow

--- tax/src/train.py
+++ tax/src/train.py
@@ -84,25 +84,6 @@ model.print_trainable_parameters()
 # trainable params: 18874368 || all params: 11154206720 || trainable%: 0.16921300163961817
 
 
-from peft import LoraConfig, get_peft_model, prepare_model_for_int8_training, TaskType
-
-# Define LoRA Config
-lora_config = LoraConfig(
- r=16,
- lora_alpha=32,
- target_modules=["q", "v"],
- lora_dropout=0.05,
- bias="none",
- task_type=TaskType.SEQ_2_SEQ_LM
-)
-# prepare int-8 model for training
-model = prepare_model_for_int8_training(model)
-
-# add LoRA adaptor
-model = get_peft_model(model, lora_config)
-model.print_trainable_parameters()
-
-# trainable params: 18874368 || all params: 11154206720 || trainable%: 0.16921300163961817
 
 # we want to ignore tokenizer pad token in the loss
 label_pad_token_id = -100
@@ -113,7 +94,26 @@ data_collator = DataCollatorForSeq2Seq(
     label_pad_token_id=label_pad_token_id,
     pad_to_multiple_of=8
 )
+training_args = Seq2SeqTrainingArguments(
+    output_dir=output_dir,
+	auto_find_batch_size=True,
+    learning_rate=1e-3, # higher learning rate
+    num_train_epochs=5,
+    logging_dir=f"{output_dir}/logs",
+    logging_strategy="steps",
+    logging_steps=500,
+    save_strategy="no",
+    report_to="tensorboard",
+)
 
+# Create Trainer instance
+trainer = Seq2SeqTrainer(
+    model=model,
+    args=training_args,
+    data_collator=data_collator,
+    train_dataset=tokenized_dataset["train"],
+)
+model.config.use_cache = False
 # train model
 trainer.train()
 
