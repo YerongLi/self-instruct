d36c901bca6606ec4b94fb9cd9939e042256c887
Yerong <yerong.li@outlook.com>
2023-12-04 07:18:32 -0600

    k shot prompt

--- tax/query3.py
+++ tax/query3.py
@@ -736,7 +736,7 @@ def predict_gpt_batch(prompts, batch_size=20):
 
 batch_size = 4
 
-predict_palm_batch(prompts, batch_size)
+# predict_palm_batch(prompts, batch_size)
 # predict_llama_batch(prompts, batch_size)
 # predict_gpt_batch(prompts)
 
