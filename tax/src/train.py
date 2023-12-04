1c21fd354aa28088a32f7dfbddb7903f064028e9
Yerong <yerong.li@outlook.com>
2023-12-04 02:55:50 -0600

    Training workflow

--- tax/src/train.py
+++ tax/src/train.py
@@ -1,15 +1,71 @@
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
-from transformers import DataCollatorForSeq2Seq
-from transformers import Seq2SeqTrainer, Seq2SeqTrainingArguments
-
-
-# huggingface hub model id
-model_id = "google/flan-t5-base"
-
-# load model from the hub
-tokenizer = AutoTokenizer.from_pretrained(model_id)
-model = AutoModelForSeq2SeqLM.from_pretrained(model_id, device_map="auto")
+from datasets import load_dataset
 from peft import LoraConfig, get_peft_model, TaskType
+from transformers import DataCollatorForSeq2Seq
+
+# Load dataset from the hub
+dataset = load_dataset("samsum")
+
+print(f"Train dataset size: {len(dataset['train'])}")
+print(f"Test dataset size: {len(dataset['test'])}")
+
+# Train dataset size: 14732
+# Test dataset size: 819
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+model_id="google/flan-t5-base"
+
+# Load tokenizer of FLAN-t5-XL
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+from datasets import concatenate_datasets
+import numpy as np
+# The maximum total input sequence length after tokenization.
+# Sequences longer than this will be truncated, sequences shorter will be padded.
+tokenized_inputs = concatenate_datasets([dataset["train"], dataset["test"]]).map(lambda x: tokenizer(x["dialogue"], truncation=True), batched=True, remove_columns=["dialogue", "summary"])
+input_lenghts = [len(x) for x in tokenized_inputs["input_ids"]]
+# take 85 percentile of max length for better utilization
+max_source_length = int(np.percentile(input_lenghts, 85))
+print(f"Max source length: {max_source_length}")
+
+# The maximum total sequence length for target text after tokenization.
+# Sequences longer than this will be truncated, sequences shorter will be padded."
+tokenized_targets = concatenate_datasets([dataset["train"], dataset["test"]]).map(lambda x: tokenizer(x["summary"], truncation=True), batched=True, remove_columns=["dialogue", "summary"])
+target_lenghts = [len(x) for x in tokenized_targets["input_ids"]]
+# take 90 percentile of max length for better utilization
+max_target_length = int(np.percentile(target_lenghts, 90))
+print(f"Max target length: {max_target_length}")
+
+
+def preprocess_function(sample,padding="max_length"):
+    # add prefix to the input for t5
+    inputs = ["summarize: " + item for item in sample["dialogue"]]
+
+    # tokenize inputs
+    model_inputs = tokenizer(inputs, max_length=max_source_length, padding=padding, truncation=True)
+
+    # Tokenize targets with the `text_target` keyword argument
+    labels = tokenizer(text_target=sample["summary"], max_length=max_target_length, padding=padding, truncation=True)
+
+    # If we are padding here, replace all tokenizer.pad_token_id in the labels by -100 when we want to ignore
+    # padding in the loss.
+    if padding == "max_length":
+        labels["input_ids"] = [
+            [(l if l != tokenizer.pad_token_id else -100) for l in label] for label in labels["input_ids"]
+        ]
+
+    model_inputs["labels"] = labels["input_ids"]
+    return model_inputs
+
+tokenized_dataset = dataset.map(preprocess_function, batched=True, remove_columns=["dialogue", "summary", "id"])
+print(f"Keys of tokenized dataset: {list(tokenized_dataset['train'].features)}")
+
+# save datasets to disk for later easy loading
+tokenized_dataset["train"].save_to_disk("data/train")
+tokenized_dataset["test"].save_to_disk("data/eval")
+
+
+model = AutoModelForSeq2SeqLM.from_pretrained(model_id, load_in_8bit=True, device_map="auto")
+
+
 
 # Define LoRA Config
 lora_config = LoraConfig(
@@ -21,15 +77,35 @@ lora_config = LoraConfig(
  task_type=TaskType.SEQ_2_SEQ_LM
 )
 # prepare int-8 model for training
-# model = prepare_model_for_int8_training(model)
+model = prepare_model_for_int8_training(model)
 
 # add LoRA adaptor
 model = get_peft_model(model, lora_config)
 model.print_trainable_parameters()
 
+# trainable params: 18874368 || all params: 11154206720 || trainable%: 0.16921300163961817
 
+
+from peft import LoraConfig, get_peft_model, prepare_model_for_int8_training, TaskType
+
+# Define LoRA Config
+lora_config = LoraConfig(
+ r=16,
+ lora_alpha=32,
+ target_modules=["q", "v"],
+ lora_dropout=0.05,
+ bias="none",
+ task_type=TaskType.SEQ_2_SEQ_LM
+)
+# prepare int-8 model for training
+model = prepare_model_for_int8_training(model)
+
+# add LoRA adaptor
+model = get_peft_model(model, lora_config)
+model.print_trainable_parameters()
 
 # trainable params: 18874368 || all params: 11154206720 || trainable%: 0.16921300163961817
+
 # we want to ignore tokenizer pad token in the loss
 label_pad_token_id = -100
 # Data collator
@@ -40,92 +116,6 @@ data_collator = DataCollatorForSeq2Seq(
     pad_to_multiple_of=8
 )
 
-from datasets import concatenate_datasets
-import numpy as np
-# The maximum total input sequence length after tokenization.
-# Sequences longer than this will be truncated, sequences shorter will be padded.
-tokenized_inputs = concatenate_datasets([dataset["train"], dataset["test"]]).map(lambda x: tokenizer(x["dialogue"], truncation=True), batched=True, remove_columns=["dialogue", "summary"])
-input_lenghts = [len(x) for x in tokenized_inputs["input_ids"]]
-# take 85 percentile of max length for better utilization
-max_source_length = int(np.percentile(input_lenghts, 85))
-print(f"Max source length: {max_source_length}")
-
-# The maximum total sequence length for target text after tokenization.
-# Sequences longer than this will be truncated, sequences shorter will be padded."
-tokenized_targets = concatenate_datasets([dataset["train"], dataset["test"]]).map(lambda x: tokenizer(x["summary"], truncation=True), batched=True, remove_columns=["dialogue", "summary"])
-target_lenghts = [len(x) for x in tokenized_targets["input_ids"]]
-# take 90 percentile of max length for better utilization
-max_target_length = int(np.percentile(target_lenghts, 90))
-print(f"Max target length: {max_target_length}")
-
-
-
-
-
-from datasets import concatenate_datasets
-import numpy as np
-# The maximum total input sequence length after tokenization.
-# Sequences longer than this will be truncated, sequences shorter will be padded.
-tokenized_inputs = concatenate_datasets([dataset["train"], dataset["test"]]).map(lambda x: tokenizer(x["dialogue"], truncation=True), batched=True, remove_columns=["dialogue", "summary"])
-input_lenghts = [len(x) for x in tokenized_inputs["input_ids"]]
-# take 85 percentile of max length for better utilization
-max_source_length = int(np.percentile(input_lenghts, 85))
-print(f"Max source length: {max_source_length}")
-
-# The maximum total sequence length for target text after tokenization.
-# Sequences longer than this will be truncated, sequences shorter will be padded."
-tokenized_targets = concatenate_datasets([dataset["train"], dataset["test"]]).map(lambda x: tokenizer(x["summary"], truncation=True), batched=True, remove_columns=["dialogue", "summary"])
-target_lenghts = [len(x) for x in tokenized_targets["input_ids"]]
-# take 90 percentile of max length for better utilization
-max_target_length = int(np.percentile(target_lenghts, 90))
-print(f"Max target length: {max_target_length}")
-
-# Define training args
-training_args = Seq2SeqTrainingArguments(
-    output_dir=output_dir,
-	auto_find_batch_size=True,
-    learning_rate=1e-3, # higher learning rate
-    num_train_epochs=5,
-    logging_dir=f"{output_dir}/logs",
-    logging_strategy="steps",
-    logging_steps=500,
-    save_strategy="no",
-    report_to="tensorboard",
-)
-
-# Create Trainer instance
-trainer = Seq2SeqTrainer(
-    model=model,
-    args=training_args,
-    data_collator=data_collator,
-    train_dataset=tokenized_dataset["train"],
-)
-model.config.use_cache = False  # silence the warnings. Please re-enable for inference!
 # train model
 trainer.train()
-# Save our LoRA model & tokenizer results
-peft_model_id="results"
-trainer.model.save_pretrained(peft_model_id)
-tokenizer.save_pretrained(peft_model_id)
-# if you want to save the base model to call
-# trainer.model.base_model.save_pretrained(peft_model_id)
 
-
-
-import torch
-from peft import PeftModel, PeftConfig
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
-
-# Load peft config for pre-trained checkpoint etc.
-peft_model_id = "results"
-config = PeftConfig.from_pretrained(peft_model_id)
-
-# load base LLM model and tokenizer
-model = AutoModelForSeq2SeqLM.from_pretrained(config.base_model_name_or_path,  load_in_8bit=True,  device_map={"":0})
-tokenizer = AutoTokenizer.from_pretrained(config.base_model_name_or_path)
-
-# Load the Lora model
-model = PeftModel.from_pretrained(model, peft_model_id, device_map={"":0})
-model.eval()
-
-print("Peft model loaded")
