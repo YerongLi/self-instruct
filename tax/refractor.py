5ea56b0ea1c1c457fb8da81e4da294bffab74448
Yerong <yerong.li@outlook.com>
2023-12-04 01:38:50 -0600

    Refractor filename

--- tax/refractor.py
+++ tax/refractor.py
@@ -82,7 +82,7 @@ def process_json_file(file_path):
                 item_value["i"] = i_cut
                 item_value["o"] = f"{i_text[cut_off_index:]}{o_text}"
                 data[item_key] = item_value
-                logging.info(data[item_key]["o"])
+                logging.info(data[item_key]["i"])
 
     # Construct the new file name by appending 'r' to the original file name
     new_file_path = f"{file_path}r"
@@ -98,7 +98,7 @@ def process_json_file(file_path):
 # Iterate through all JSON files in the directory
 for filename in os.listdir(datapath):
     if filename.endswith(".json"):
-        # filename='siblings_0shot_300.json' fixed
+        filename='siblings_0shot_300.json' fixed
         file_path = os.path.join(datapath, filename)
         process_json_file(file_path)
         break
