eab0f9bc39f7859d841deea1d476110185dcf388
Yerong <yerong.li@outlook.com>
2023-12-05 13:10:37 -0600

    Upload the chat

--- chat/summary.py
+++ chat/summary.py
@@ -3,6 +3,9 @@ import os
 directory_path = "sampled"  # Change this to the desired directory path
 
 # Check if the directory exists
+prompt = []
+prefix = "Could you give a summary on the following dialogue, keeping [PERSON], [LOC] as privacy tokens?\n"
+suffix = "\nSummary:\n"
 if os.path.exists(directory_path) and os.path.isdir(directory_path):
     # Walk through the directory and its immediate subdirectories (depth=1)
     for root, dirs, files in os.walk(directory_path):
@@ -13,6 +16,7 @@ if os.path.exists(directory_path) and os.path.isdir(directory_path):
                     file_path = os.path.join(root, file)
                     with open(file_path, 'r') as txt_file:
                         file_contents = txt_file.read()
-                        print(f"Contents of {file_path}:\n{file_contents}\n{'-' * 50}")
+                        prompts.append({'prompt': prefix + file_contents + suffix, 'filename' : filename})
+                        # print(f"Contents of {file_path}:\n{file_contents}\n{'-' * 50}")
 else:
     print(f"The directory '{directory_path}' does not exist.")
