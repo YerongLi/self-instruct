8e903f14acdff65bd40b8c71d0c33f1c00210c55
Yerong <yerong.li@outlook.com>
2023-12-05 13:06:14 -0600

    Upload the chat

--- chat/summary.py
+++ chat/summary.py
@@ -6,7 +6,7 @@ directory_path = "sampled"  # Change this to the desired directory path
 if os.path.exists(directory_path) and os.path.isdir(directory_path):
     # Get a list of all *.txt files in the directory
     txt_files = [file for file in os.listdir(directory_path) if file.endswith(".txt")]
-
+    print(txt_files)
     # Read and print the contents of each *.txt file
     for txt_file in txt_files:
         file_path = os.path.join(directory_path, txt_file)
