8e2e53bd29373d096caf628a306ee447f90ead91
Yerong <yerong.li@outlook.com>
2023-12-05 13:11:45 -0600

    Upload the chat

--- chat/summary.py
+++ chat/summary.py
@@ -20,3 +20,5 @@ if os.path.exists(directory_path) and os.path.isdir(directory_path):
                         # print(f"Contents of {file_path}:\n{file_contents}\n{'-' * 50}")
 else:
     print(f"The directory '{directory_path}' does not exist.")
+for prompt in prompts[:10]:
+    print(prompt)
