d424b0688400a0ffc17fede6e9d68359deb7a579
Yerong <yerong.li@outlook.com>
2023-12-05 13:11:11 -0600

    Upload the chat

--- chat/summary.py
+++ chat/summary.py
@@ -16,7 +16,7 @@ if os.path.exists(directory_path) and os.path.isdir(directory_path):
                     file_path = os.path.join(root, file)
                     with open(file_path, 'r') as txt_file:
                         file_contents = txt_file.read()
-                        prompts.append({'prompt': prefix + file_contents + suffix, 'filename' : filename})
+                        prompts.append({'prompt': prefix + file_contents + suffix, 'filename' : file_path})
                         # print(f"Contents of {file_path}:\n{file_contents}\n{'-' * 50}")
 else:
     print(f"The directory '{directory_path}' does not exist.")
