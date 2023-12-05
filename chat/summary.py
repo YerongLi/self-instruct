f0f032ba348e8a7b7de1928b3c643ed6b4b52d4c
Yerong <yerong.li@outlook.com>
2023-12-05 13:04:56 -0600

    Upload the chat

---
+++ chat/summary.py
@@ -0,0 +1,17 @@
+import os
+
+directory_path = "sampled"  # Change this to the desired directory path
+
+# Check if the directory exists
+if os.path.exists(directory_path) and os.path.isdir(directory_path):
+    # Get a list of all *.txt files in the directory
+    txt_files = [file for file in os.listdir(directory_path) if file.endswith(".txt")]
+
+    # Read and print the contents of each *.txt file
+    for txt_file in txt_files:
+        file_path = os.path.join(directory_path, txt_file)
+        with open(file_path, 'r') as file:
+            file_contents = file.read()
+            print(f"Contents of {txt_file}:\n{file_contents}\n{'-' * 50}")
+else:
+    print(f"The directory '{directory_path}' does not exist.")
