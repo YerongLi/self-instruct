3525b1a1177ff3bda00dd0ce6155ac773bd0b745
Yerong <yerong.li@outlook.com>
2023-12-07 03:46:09 -0600

    Upload

--- tax/train.py
+++ tax/train.py
@@ -78,7 +78,9 @@ filename = f"{datapath}/dataset{TOTAL}.json"
 print(filename)
 with open(filename, "r") as f:
         dataset = json.load(f)
-dataset = load_dataset("samsum")
+# dataset = load_dataset("samsum")
+dataset= DatasetDict.load_from_disk(f"{datapath}/dataset{TOTAL}.data")
+
 print(dataset)
 class SaveBestModelCallback(TrainerCallback):
     def __init__(self, output_dir):
