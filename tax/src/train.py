d4419566ce58f26fd117d725a9e02bea0d320a06
Yerong <yerong.li@outlook.com>
2023-12-07 01:53:15 -0600

    Update the train.py

--- tax/src/train.py
+++ tax/src/train.py
@@ -25,9 +25,10 @@ class SaveBestModelCallback(TrainerCallback):
 
     def on_evaluate(self, args, state, control, model, tokenizer, **kwargs):
         eval_loss = control.metrics['eval_loss']
+        print(self.output_dir)
+        print(eval_loss, self.best_eval_loss)
         if eval_loss < self.best_eval_loss:
             # Save the model if the evaluation loss improves
-            print(self.output_dir)
             model.save_pretrained(f"{self.output_dir}/best/")
             self.best_eval_loss = eval_loss
             print(f"Model saved with eval loss: {eval_loss}")
