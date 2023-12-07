ec7173e2afdd2777a6e8da167bebbc93903dfff3
Yerong <yerong.li@outlook.com>
2023-12-07 03:55:04 -0600

    Upload

--- tax/merge.py
+++ tax/merge.py
@@ -18,7 +18,7 @@ import os
 
 from transformers import LlamaForCausalLM, AutoTokenizer, LogitsProcessorList
 from torch.utils.data import DataLoader
-from datasets import Dataset
+from datasets import Dataset, DatasetDict
 openai_api_key = os.environ.get("OPENAI")
 
 if not openai_api_key:
