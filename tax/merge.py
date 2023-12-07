d02a88fba7aca2a1d9b0f975d24531ccafa86bb2
Yerong <yerong.li@outlook.com>
2023-12-07 03:17:05 -0600

    Upload

---
+++ tax/merge.py
@@ -0,0 +1,80 @@
+import argparse
+import json
+import logging
+import hashlib
+import os
+import pickle
+import random
+import shutil
+import tqdm
+# import networkx as nx
+import torch
+import requests
+import json
+import time
+from openai import OpenAI
+import google.generativeai as palm
+import os
+
+from transformers import LlamaForCausalLM, AutoTokenizer, LogitsProcessorList
+from torch.utils.data import DataLoader, Dataset
+openai_api_key = os.environ.get("OPENAI")
+
+if not openai_api_key:
+    print("OpenAI API key not found in environment variables.")
+client = OpenAI(api_key=openai_api_key)
+LOGFILE='output.log'
+palm.configure(api_key=os.environ['PALM'])
+# from transformers import LlamaForCausalLM, AutoTokenizer, LogitsProcessorList
+# from torch.utils.data import DataLoader, Dataset
+parser = argparse.ArgumentParser(description="Your script description")
+# Add the configuration file argument
+parser.add_argument("config_file", type=str, help="Path to the configuration file")
+parser.add_argument("TOTAL", type=int, default=700, nargs="?", help="Number of total items to process")
+def HASH(input_string):
+    # Use SHA-256 for deterministic hashing
+    hash_object = hashlib.sha256(input_string.encode())
+    hash_value = int.from_bytes(hash_object.digest(), byteorder='big')
+
+    return str(hash_value)
+args = parser.parse_args()
+TOTAL = args.TOTAL
+
+config_file = args.config_file
+# Read the configuration file
+with open(config_file) as f:
+    config = json.load(f)
+
+# Get the configuration values
+# Extract the base filename without the ".taxo" extension
+datapath = config['taxofilename'].split('/')[:-1]
+datapath = '/'.join(datapath)
+
+print(datapath)
+filenames=[
+f"{datapath}/prediction_kshot_{TOTAL}.json",
+f"{datapath}/siblings_kshot_{TOTAL}.json",
+f"{datapath}/parent_kshot_{TOTAL}.json",
+]
+dataset = {'train' : [], 'test' : None}
+for filename in filenames:
+    with open(filename, "r") as f:
+        predictions = json.load(f)
+
+    # Iterate through keys in predictions and append 'i' and 'o' values to lists
+    for key in tqdm.tqdm(predictions):
+        entry = predictions[key]
+        if idx % 4 == 0:
+            dataset['test']['i'].append(entry['i'])
+            dataset['test']['o'].append(entry['o'])
+        else:
+            dataset['train']['i'].append(entry['i'])
+            dataset['train']['o'].append(entry['o'])
+
+output_file_path = f"dataset{TOTAL}.json"
+
+# Dump the dataset to the JSON file
+with open(output_file_path, "w") as json_file:
+    json.dump(dataset, json_file, indent=2)
+
+print(f"Dataset saved to {output_file_path}")
