8e4f7d2b496e8d2ea2eb03931af0f7582e91ab2f
Yerong <yerong.li@outlook.com>
2023-12-08 07:02:01 -0600

    abtest

--- tax/eval.py
+++ tax/eval.py
@@ -119,7 +119,7 @@ def predict_next_token_batch(prompts, batch_size=10):
         inputs = tokenizer([sentence for sentence in batch_prompts], return_tensors="pt", padding=True)
 
         output_sequences = model.generate(
-            input_ids=inputs["input_ids"],to(device),
+            input_ids=inputs["input_ids"].to(device),
             attention_mask=inputs["attention_mask"].to(device),
             max_new_tokens=1,
             do_sample=False,  # disable sampling to test if batching affects output
