615b8c9798d98fff84dec8417cddc82191bcf389
Yerong <yerong.li@outlook.com>
2023-12-08 02:05:50 -0600

    Downgrade the time

--- tax/query4-k.py
+++ tax/query4-k.py
@@ -755,7 +755,7 @@ def predict_gpt_batch(prompts, batch_size=20):
                 frequency_penalty=0,
                 presence_penalty=0
             )
-            time.sleep(6)
+            time.sleep(16)
 
 
             # Access individual responses in the list
