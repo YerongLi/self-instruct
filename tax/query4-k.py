d6b84580beeda561c0b3c39703916cc394ae9a15
Yerong <yerong.li@outlook.com>
2023-12-11 10:28:08 -0600

    Upload the evaluation script

--- tax/query4-k.py
+++ tax/query4-k.py
@@ -572,7 +572,7 @@ for iteration, edge in tqdm.tqdm(enumerate(core_graph.edges()), total=core_graph
         selected_predecessors = random.sample(filtered_predecessors, min(3, len(filtered_predecessors)))
         selected_predecessors = set(selected_predecessors)
         selected_predecessors.add(f_grandparent)
-        print(f_grandparent in core_graph.predecessors(f_parent_), f_grandparent_ not in parents_of_parents_of_kid)
+        print(f_grandparent in core_graph.predecessors(f_parent_), f_grandparent not in parents_of_parents_of_kid)
         pre_labels = [get_first_label_without_n(definitions[node]['label']) for node in selected_predecessors]
         q_pre_labels = [f'"{label}"' for label in pre_labels]
         del pre_labels
