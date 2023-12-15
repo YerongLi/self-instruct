965de22ae67c3be04a3469a9d1aea01fd39df559
Yerong <yerong.li@outlook.com>
2023-12-15 08:07:34 -0600

    Upload

--- chat/insert_number.py
+++ chat/insert_number.py
@@ -2,11 +2,12 @@ import json
 import os
 
 # Define the file path
-file_path = 'police1.json'  # Replace with the actual file path
+input_file_path = 'police1.json'  # Replace with the actual file path
+output_file_path = 'police2.json'  # Replace with the actual file path
 
 # Read JSON file line by line
-with open(file_path, 'r') as file:
-    for line in file:
+with open(input_file_path, 'r') as input_file:
+    for line in input_file:
         try:
             # Parse each line as JSON
             entry = json.loads(line)
@@ -28,13 +29,19 @@ with open(file_path, 'r') as file:
                         elif line.startswith("Admin"):
                             admin_count += 1
 
-                # Print the counts for the current entry
-                # print(f"File: {filename}")
-                # print(f"User lines count: {user_count}")
-                # print(f"Admin lines count: {admin_count}")
-                # print("\n")
+                # Add "his_len" field to the entry
+                entry['his_len'] = user_count + admin_count
+
+                # Append the modified entry to the list
+                modified_entries.append(entry)
             else:
                 print(f"File not found: {filename}")
 
         except json.JSONDecodeError:
             print("Error decoding JSON. Check the format of the JSON lines.")
+
+# Dump the modified entries into a new JSON file
+with open(output_file_path, 'w') as output_file:
+    for modified_entry in modified_entries:
+        json.dump(modified_entry, output_file)
+        output_file.write('\n')
