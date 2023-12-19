51198327aab7434de7c96f3a9e48f632c73849df
Yerong <yerong.li@outlook.com>
2023-12-19 09:37:55 -0600

    split.sh

--- chat/split.sh
+++ chat/split.sh
@@ -1,1 +1,1 @@
-awk 'NR % 4 == 0 { print > "police1.json" } NR % 4 != 0 { print > "police.json" }' input_file.json
+awk 'NR % 4 == 0 { print > "police1.json" } NR % 4 != 0 { print > "police.json" }' police-full.json
