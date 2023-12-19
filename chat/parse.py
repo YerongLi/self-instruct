43bbabf6c6712828f9ac417c3b099c95f52f2bce
Yerong <yerong.li@outlook.com>
2023-12-19 09:28:35 -0600

    parse.py file

--- chat/parse.py
+++ chat/parse.py
@@ -1,40 +1,20 @@
-import json
 import pandas as pd
+import json
 
 # Specify the correct encoding of your CSV files
 encoding = 'latin-1'  # or 'ISO-8859-1' or other suitable encoding
 
-# Read the first CSV file containing chat data
-chat_df = pd.read_csv('chat.csv', encoding=encoding)
-
-# Read the second CSV file containing event details
+# Read the event CSV file to create a hashmap from event ID to event category
 event_df = pd.read_csv('event.csv', encoding=encoding)
+event_type_map = dict(zip(event_df['Anonymized Event ID'], event_df['Eventcategory']))
+
+# Read the chat CSV file containing chat data
+chat_df = pd.read_csv('chat.csv', encoding=encoding)
 
 # Create a dictionary to store the result
 result_dict = {}
 
 # Iterate through rows in the chat dataframe
-for index, row in chat_df.iterrows():
-    event_id = row['Anonymized Eventid']
-    chat_history = row['Chat']
-
-    # Check if the event_id is already in the dictionary
-    if event_id in result_dict:
-        # Append the chat history to the existing list
-        result_dict[event_id]['chat'].append(chat_history)
-    else:
-        # Create a new dictionary entry for the event_id
-        result_dict[event_id] = {'chat': [chat_history]}
-current_event_type = None
-# Iterate through rows in the event dataframe to add event category
-for index, row in event_df.iterrows():
-    event_id = row['Anonymized Event ID']
-    event_category = row['Eventcategory']
-
-    # Check if the event_id is in the result_dict
-    if event_id in result_dict:
-        # Add the event category to the dictionary entry
-        result_dict[event_id]['type'] = event_category
 for index, row in chat_df.iterrows():
     event_id = row['Anonymized Eventid']
     chat_history = row['Chat']
@@ -52,7 +32,7 @@ for index, row in chat_df.iterrows():
         # Extract information for the police.json entry
         instruction = result_dict[event_id]['chat'][-1]
         history = result_dict[event_id]['chat'][:-1]
-        event_type = current_event_type  # Use the previously stored event type
+        event_type = event_type_map.get(event_id, 'unknown')  # Get event category from the hashmap
 
         # Dump the entry to police.json
         entry = {
@@ -68,4 +48,3 @@ for index, row in chat_df.iterrows():
 # Print or store the result_dict as needed
 for event_id, data in result_dict.items():
     print(f"Event {event_id}: {data}")
-
