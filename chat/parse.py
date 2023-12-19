933c48177ed22bdb6af33f5c98de07a680202650
Yerong <yerong.li@outlook.com>
2023-12-19 09:13:09 -0600

    parse.py file

--- chat/parse.py
+++ chat/parse.py
@@ -1,24 +1,41 @@
 import pandas as pd
 
-# Read the CSV file
-df = pd.read_csv('chat.csv', encoding='latin-1')
+# Specify the correct encoding of your CSV files
+encoding = 'latin-1'  # or 'ISO-8859-1' or other suitable encoding
 
-# Initialize a dictionary to store conversations for each event
-event_conversations = {}
+# Read the first CSV file containing chat data
+chat_df = pd.read_csv('chat.csv', encoding=encoding)
 
-# Iterate through rows in the dataframe
-for index, row in df.iterrows():
+# Read the second CSV file containing event details
+event_df = pd.read_csv('event.csv', encoding=encoding)
+
+# Create a dictionary to store the result
+result_dict = {}
+
+# Iterate through rows in the chat dataframe
+for index, row in chat_df.iterrows():
     event_id = row['Anonymized Eventid']
     chat_history = row['Chat']
 
     # Check if the event_id is already in the dictionary
-    if event_id in event_conversations:
+    if event_id in result_dict:
         # Append the chat history to the existing list
-        event_conversations[event_id].append(chat_history)
+        result_dict[event_id]['chat'].append(chat_history)
     else:
-        # Create a new list for the event_id and add the chat history
-        event_conversations[event_id] = [chat_history]
+        # Create a new dictionary entry for the event_id
+        result_dict[event_id] = {'chat': [chat_history]}
+
+# Iterate through rows in the event dataframe to add event category
+for index, row in event_df.iterrows():
+    event_id = row['Anonymized Eventid']
+    event_category = row['Eventcategory']
+
+    # Check if the event_id is in the result_dict
+    if event_id in result_dict:
+        # Add the event category to the dictionary entry
+        result_dict[event_id]['type'] = event_category
+
 # Print or store the result as needed
-print(len(event_conversations))
-# for event_id, conversations in event_conversations.items():
-#     print(f"Event {event_id}: {conversations}")
+for event_id, data in result_dict.items():
+    print(f"Event {event_id}: {data}")
+    break
