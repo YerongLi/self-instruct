f759b3e66336dff9b3766789891b675d116dd233
Yerong <yerong.li@outlook.com>
2023-12-21 10:23:32 -0600

    Update

--- chat/parse.py
+++ chat/parse.py
@@ -12,7 +12,7 @@ if os.path.exists(filename):
     # Remove the file
     os.remove(filename)
 # Read the event CSV file to create a hashmap from event ID to event category
-event_df = pd.read_csv('event.csv', encoding=encoding, error_bad_lines=False)
+event_df = pd.read_csv('event.csv', encoding=encoding, on_bad_lines='skip')
 # event_df = pickle.load(open('df_event.pkl', 'rb'))
 # event_df = pd.read_pickle('df_event.pkl')
 event_type_map = {}
