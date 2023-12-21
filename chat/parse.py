86d599fcac0050d9ea43f2400b5b5f44b9844459
Yerong <yerong.li@outlook.com>
2023-12-21 06:41:40 -0600

    Update

--- chat/parse.py
+++ chat/parse.py
@@ -10,7 +10,7 @@ if os.path.exists(filename):
     # Remove the file
     os.remove(filename)
 # Read the event CSV file to create a hashmap from event ID to event category
-event_df = pd.read_csv('event.csv', encoding=encoding, quotechar='"')
+event_df = pd.read_csv('event.csv', encoding=encoding)
 event_type_map = {}
 
 # Iterate over rows in the event_df DataFrame
@@ -20,14 +20,14 @@ for index, row in event_df.iterrows():
     event_category = row['Eventcategory (group)']
     # if event_id == 2073482:
         # print(event_category)
-    if index != 4413 and index != 4414 : continue
-    print('id')
-    print(event_id)
-    print(event_category)
-    print(row)
-    print('==========')
-    # Assign the event category to the event ID in the map
-    event_type_map[event_id] = event_category
+
+    # if index != 4413 and index != 4414 : continue
+    # print('id')
+    # print(event_id)
+    if event_category not in {'DrugsAlcohol', 'HarassmentAbuse', 'MentalHealth', 'TheftLostItem', 'SuspiciousActivity', 'EmergencyMessage'}: continue
+        print(event_category)
+        print('==========')
+        event_type_map[event_id] = event_category
 print(event_type_map[1992077])
 # Read the chat CSV file containing chat data
 chat_df = pd.read_csv('chat.csv', encoding=encoding)
