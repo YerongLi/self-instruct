ed9f86e7286063bf3c3e44ce3d13b2f9873a1067
Yerong Li <yerong.li@outlook.com>
2024-01-08 21:08:40 -0500

    Chat

--- chat/parse.py
+++ chat/parse.py
@@ -43,7 +43,7 @@ event_df.to_csv('event_df.csv', index=False)
 print('Creating chat_df.csv file')
 chat_df = pd.read_pickle('df_chat.pkl')
 chat_df['Chat Date'] = pd.to_datetime(chat_df['Chat Date'])
-
+print(chat_df['Chat Date'])
 chat_df.sort_values(by=['Anonymized Eventid', 'Chat Date'], inplace=True)
 unique_chat_roles = set(chat_df['Chattype'])
 print(unique_chat_roles)
